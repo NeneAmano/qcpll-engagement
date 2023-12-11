0902f1fe481f7016ac37fc92d3aa05091bc293a4
n67h <night67hunter@gmail.com>
2023-12-11 17:53:16 +0800

    Modify scripts

--- app/admin/includes/head.php
+++ app/admin/includes/head.php
@@ -13,6 +13,9 @@
     <!-- latest bootstrap cdn -->
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"> -->
     <link rel="stylesheet" href="<?= ROOT ?>/assets/css/style.css">
-    
+    <!-- jquery datatable css cdn -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.css">
+    <!-- font-awesome cdn -->
+    <script src="https://kit.fontawesome.com/3481525a72.js" crossorigin="anonymous"></script>
     <title>QCPL</title>
 </head>
