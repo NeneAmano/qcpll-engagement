f6f716cddb3ae41824d7e690055000e23873f031
BERT151 <102898303+BERT151@users.noreply.github.com>
2024-01-17 14:36:13 +0800

    adding log out function

--- public/portal.php
+++ public/portal.php
@@ -1,9 +1,40 @@
 <?php 
     require_once("../app/includes/header.php");
 ?>
-<body>
 
+<body>
+<style>
+    .dropdown{
+        max-width: 10%;
+        position: fixed;
+        display: flex;
+        justify-content: flex-start;
+    }
+    .dropdown:hover>.dropdown-menu {
+        display: block;
+}
+
+.dropdown>.dropdown-toggle:active {
+  /*Without this, clicking will make it sticky*/
+    pointer-events: none;
+}
+</style>
     <section id="swup" class="transtion-fade">
+    <?php  if(isset($_SESSION['user_id'])){
+        $sql = "SELECT username FROM users";
+        $res = mysqli_query($conn,$sql);
+        while($row = mysqli_fetch_assoc($res)){
+            $username = $row['username'];
+            echo '<div class="dropdown">';
+                echo '<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                    '.$username.'
+                </button>';
+                    echo '<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">';
+                        echo '<li><a class="dropdown-item" href="../app/includes/logout.php">Log out</a></li>';
+                    echo '</ul>';
+            echo '</div>';
+        }
+       }?>
     <div class="logo">
             <img src="./images/qclogo.jpg" alt="">
             <div class="title">
@@ -26,8 +57,13 @@
                <!-- <a href="feedback.php" class="FeedbackBtn">To Feedback</a> -->
             </div>
         </div>
-    </section>
+        
+
+    
+
     <script src="https://unpkg.com/swup@4"></script>
     <script src="script.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
 </html>
