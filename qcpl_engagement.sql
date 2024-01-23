1703bc2a4719b2218a9338dd1a5d2c100508a7b0
n67h <night67hunter@gmail.com>
2024-01-23 10:24:50 +0800

    Update database

--- qcpl_engagement.sql
+++ qcpl_engagement.sql
@@ -28,25 +28,13 @@ CREATE TABLE IF NOT EXISTS `age` (
   PRIMARY KEY (`age_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
-
--- Dumping structure for table qcpl_engagement.choices
-CREATE TABLE IF NOT EXISTS `choices` (
-  `choices_id` int NOT NULL AUTO_INCREMENT,
-  `question_id` int DEFAULT NULL,
-  `emoji_id` int DEFAULT NULL,
-  `choices` varchar(255) DEFAULT NULL,
-  `is_deleted` tinyint NOT NULL DEFAULT '0',
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`choices_id`),
-  KEY `question_id` (`question_id`),
-  KEY `emoji_id` (`emoji_id`),
-  CONSTRAINT `choices_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `questions` (`question_id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  CONSTRAINT `choices_ibfk_2` FOREIGN KEY (`emoji_id`) REFERENCES `emoji` (`emoji_id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- Data exporting was unselected.
+-- Dumping data for table qcpl_engagement.age: ~5 rows (approximately)
+INSERT INTO `age` (`age_id`, `age_range`, `created_at`, `updated_at`) VALUES
+	(1, '0-12', '2023-12-06 15:20:43', '2023-12-06 15:20:43'),
+	(2, '13-21', '2023-12-06 15:20:58', '2023-12-06 15:20:58'),
+	(3, '22-35', '2023-12-06 15:21:15', '2023-12-06 15:21:15'),
+	(4, '36-59', '2023-12-06 15:21:25', '2023-12-06 15:21:25'),
+	(5, '60 above', '2023-12-06 15:21:37', '2023-12-06 15:21:52');
 
 -- Dumping structure for table qcpl_engagement.client
 CREATE TABLE IF NOT EXISTS `client` (
@@ -59,54 +47,123 @@ CREATE TABLE IF NOT EXISTS `client` (
   `gender` varchar(255) NOT NULL,
   `education` varchar(255) NOT NULL,
   `occupation` varchar(255) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '0',
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`client_id`),
   KEY `age_id` (`age_id`),
   CONSTRAINT `client_ibfk_1` FOREIGN KEY (`age_id`) REFERENCES `age` (`age_id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=75 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table qcpl_engagement.client: ~4 rows (approximately)
+INSERT INTO `client` (`client_id`, `f_name`, `m_name`, `l_name`, `suffix`, `age_id`, `gender`, `education`, `occupation`, `status`, `created_at`, `updated_at`) VALUES
+	(1, 'Bogart', 'Pederico', 'Pedring', NULL, 5, 'Male', 'Elementary Graduate', 'Unemployed', 0, '2024-01-22 05:03:51', '2024-01-22 05:03:57'),
+	(4, 'asdasdasd', '', 'asdsadasd', '', 5, 'Female', 'Elementary Graduate', 'Unemployed', 0, '2024-01-22 06:15:14', '2024-01-22 06:15:14'),
+	(5, 'qweqweqwe', 'qeqe', 'qweqeq', 'qweqe', 3, 'Female', 'HighSchool Level', 'Unemployed', 0, '2024-01-22 06:16:18', '2024-01-22 06:16:18'),
+	(6, 'zczczczxczx', '', 'czxczxczxczxc', '', 2, 'Male', 'HighSchool Level', 'Employed', 0, '2024-01-22 06:16:54', '2024-01-22 06:16:54');
 
 -- Dumping structure for table qcpl_engagement.emoji
 CREATE TABLE IF NOT EXISTS `emoji` (
   `emoji_id` int NOT NULL AUTO_INCREMENT,
-  `image` varchar(255) NOT NULL,
-  `is_deleted` tinyint NOT NULL DEFAULT '0',
+  `image_path` varchar(255) NOT NULL,
+  `unicode` varchar(255) NOT NULL,
+  `positive` decimal(4,3) NOT NULL,
+  `neutral` decimal(4,3) NOT NULL,
+  `negative` decimal(4,3) NOT NULL,
+  `sentiment_score` decimal(4,3) NOT NULL,
+  `value` varchar(255) NOT NULL,
+  `unicode_name` varchar(255) NOT NULL,
+  `remarks` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`emoji_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table qcpl_engagement.emoji: ~6 rows (approximately)
+INSERT INTO `emoji` (`emoji_id`, `image_path`, `unicode`, `positive`, `neutral`, `negative`, `sentiment_score`, `value`, `unicode_name`, `remarks`, `created_at`, `updated_at`) VALUES
+	(1, 'public/assets/images/EMOJI/1.png', '0x1f600', 0.654, 0.260, 0.086, 0.568, 'Very Satisfied', 'GRINNING FACE', 'Often conveys general pleasure and good cheer or humor.', '2024-01-22 08:28:28', '2024-01-22 14:49:40'),
+	(2, 'public/assets/images/EMOJI/2.png', '0x1f60a', 0.704, 0.237, 0.060, 0.644, 'Satisfied', 'SMILING FACE WITH SMILING EYES', 'Often expresses genuine happiness and warm, positive feelings.', '2024-01-22 08:36:20', '2024-01-22 14:49:48'),
+	(3, 'public/assets/images/EMOJI/3.png', '0x1f610', 0.165, 0.282, 0.553, -0.388, 'Neutral', 'NEUTRAL FACE', 'Often used to convey mild irritation and concern or a deadpan sense of humor.', '2024-01-22 08:39:26', '2024-01-22 14:49:54'),
+	(4, 'public/assets/images/EMOJI/4.png', '0x1f614', 0.318, 0.219, 0.464, -0.146, 'Dissatisfied', 'PENSIVE FACE', 'May convey a variety of sad emotions, including feeling disappointed, hurt, or lonely', '2024-01-22 08:42:19', '2024-01-22 14:50:04'),
+	(5, 'public/assets/images/EMOJI/5.png', '0x1f620', 0.265, 0.172, 0.564, -0.299, 'Very Dissatisfied', 'ANGRY FACE', 'Conveys varying degrees of anger, from grumpiness and irritation to disgust and outrage.', '2024-01-22 08:43:54', '2024-01-22 14:50:16'),
+	(6, 'test', '0', 0.000, 0.000, 0.000, 0.000, 'test', 'test', 'test', '2024-01-23 00:16:24', '2024-01-23 00:16:24');
 
 -- Dumping structure for table qcpl_engagement.feedback
 CREATE TABLE IF NOT EXISTS `feedback` (
   `feedback_id` int NOT NULL AUTO_INCREMENT,
+  `client_id` int DEFAULT NULL,
   `question_id` int DEFAULT NULL,
-  `choices_id` int DEFAULT NULL,
+  `emoji_id` int DEFAULT NULL,
+  `text_feedback` text,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`feedback_id`),
+  KEY `client_id` (`client_id`),
   KEY `question_id` (`question_id`),
-  KEY `choices_id` (`choices_id`),
-  CONSTRAINT `feedback_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `questions` (`question_id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  CONSTRAINT `feedback_ibfk_2` FOREIGN KEY (`choices_id`) REFERENCES `choices` (`choices_id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `emoji_id` (`emoji_id`),
+  CONSTRAINT `feedback_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `client` (`client_id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `feedback_ibfk_2` FOREIGN KEY (`question_id`) REFERENCES `questions` (`question_id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `feedback_ibfk_3` FOREIGN KEY (`emoji_id`) REFERENCES `emoji` (`emoji_id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table qcpl_engagement.feedback: ~0 rows (approximately)
 
 -- Dumping structure for table qcpl_engagement.questions
 CREATE TABLE IF NOT EXISTS `questions` (
   `question_id` int NOT NULL AUTO_INCREMENT,
-  `question` varchar(255) NOT NULL,
+  `qt_id` int DEFAULT NULL,
+  `qc_id` int DEFAULT NULL,
+  `english_question` varchar(255) NOT NULL,
+  `tagalog_question` varchar(255) NOT NULL,
   `is_deleted` tinyint NOT NULL DEFAULT '0',
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`question_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`question_id`),
+  KEY `qt_id` (`qt_id`),
+  KEY `qc_id` (`qc_id`),
+  CONSTRAINT `questions_ibfk_1` FOREIGN KEY (`qt_id`) REFERENCES `question_type` (`qt_id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `questions_ibfk_2` FOREIGN KEY (`qc_id`) REFERENCES `question_category` (`qc_id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table qcpl_engagement.questions: ~7 rows (approximately)
+INSERT INTO `questions` (`question_id`, `qt_id`, `qc_id`, `english_question`, `tagalog_question`, `is_deleted`, `created_at`, `updated_at`) VALUES
+	(1, 1, 1, 'How was your experience with our staff?', 'Kumusta ang iyong karanasan sa aming mga tauhan?', 0, '2024-01-22 08:57:07', '2024-01-22 08:57:19'),
+	(2, 1, 1, 'Is our staff helpful?', 'Matulungin ba ang aming mga tauhan?', 0, '2024-01-22 08:57:55', '2024-01-22 08:57:55'),
+	(3, 1, 2, 'How was our service?', 'Kumusta ang aming serbisyo?', 0, '2024-01-22 08:59:29', '2024-01-22 08:59:35'),
+	(4, 1, 2, 'Are you satisfied with the service you received?', 'Nasiyahan ka ba sa iyong serbisyong natanggap?', 0, '2024-01-22 09:01:16', '2024-01-22 09:01:16'),
+	(5, 1, 3, 'Is the facility clean?', 'Malinis ba ang pasilidad?', 0, '2024-01-22 09:02:02', '2024-01-22 09:02:02'),
+	(6, 1, 3, 'Do you feel comfortable while waiting?', 'Komportable ka ba habang naghihintay?', 0, '2024-01-22 09:02:42', '2024-01-22 09:02:42'),
+	(7, 2, 4, 'How was your overall experience with our system?', 'Kumusta ang iyong pangkalahatang karanasan sa aming sistema?', 0, '2024-01-22 09:05:17', '2024-01-22 09:05:17');
+
+-- Dumping structure for table qcpl_engagement.question_category
+CREATE TABLE IF NOT EXISTS `question_category` (
+  `qc_id` int NOT NULL AUTO_INCREMENT,
+  `question_category` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`qc_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table qcpl_engagement.question_category: ~4 rows (approximately)
+INSERT INTO `question_category` (`qc_id`, `question_category`, `created_at`, `updated_at`) VALUES
+	(1, 'Staff', '2024-01-22 06:54:45', '2024-01-22 06:54:45'),
+	(2, 'Service', '2024-01-22 06:54:53', '2024-01-22 06:54:53'),
+	(3, 'Facility', '2024-01-22 06:55:01', '2024-01-22 06:55:01'),
+	(4, 'System', '2024-01-22 09:03:22', '2024-01-22 09:03:22');
+
+-- Dumping structure for table qcpl_engagement.question_type
+CREATE TABLE IF NOT EXISTS `question_type` (
+  `qt_id` int NOT NULL AUTO_INCREMENT,
+  `question_type` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`qt_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table qcpl_engagement.question_type: ~2 rows (approximately)
+INSERT INTO `question_type` (`qt_id`, `question_type`, `created_at`, `updated_at`) VALUES
+	(1, 'Emoji-based', '2024-01-22 08:51:53', '2024-01-22 08:51:53'),
+	(2, 'Text-based', '2024-01-22 08:52:02', '2024-01-22 08:52:02');
 
 -- Dumping structure for table qcpl_engagement.queue
 CREATE TABLE IF NOT EXISTS `queue` (
@@ -115,24 +172,38 @@ CREATE TABLE IF NOT EXISTS `queue` (
   `queue_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`queue_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table qcpl_engagement.queue: ~1 rows (approximately)
+INSERT INTO `queue` (`queue_id`, `total_queue`, `queue_date`, `updated_at`) VALUES
+	(1, 5, '2024-01-21 16:00:00', '2024-01-22 06:16:54');
 
 -- Dumping structure for table qcpl_engagement.queue_details
 CREATE TABLE IF NOT EXISTS `queue_details` (
   `qd_id` int NOT NULL AUTO_INCREMENT,
   `client_id` int DEFAULT NULL,
+  `queue_number` varchar(255) NOT NULL,
   `service` varchar(255) NOT NULL,
-  `queue_no` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `status` tinyint(1) DEFAULT '0',
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`qd_id`),
   KEY `client_id` (`client_id`),
   CONSTRAINT `queue_details_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `client` (`client_id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=73 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table qcpl_engagement.queue_details: ~10 rows (approximately)
+INSERT INTO `queue_details` (`qd_id`, `client_id`, `queue_number`, `service`, `status`, `created_at`, `updated_at`) VALUES
+	(1, 1, 'P-00001', 'NBI', 0, '2024-01-22 05:04:14', '2024-01-22 05:05:58'),
+	(2, 1, 'P-00002', 'Police', 0, '2024-01-22 05:33:11', '2024-01-22 05:33:11'),
+	(5, 4, 'P-00003', 'NBI', 0, '2024-01-22 06:15:14', '2024-01-22 06:15:14'),
+	(6, 4, 'P-00003', 'Police', 0, '2024-01-22 06:15:14', '2024-01-22 06:15:14'),
+	(7, 4, 'P-00003', 'test1', 0, '2024-01-22 06:15:14', '2024-01-22 06:15:14'),
+	(8, 4, 'P-00003', 'test2', 0, '2024-01-22 06:15:14', '2024-01-22 06:15:14'),
+	(9, 4, 'P-00003', 'test3', 0, '2024-01-22 06:15:14', '2024-01-22 06:15:14'),
+	(10, 5, 'P-00004', 'NBI', 0, '2024-01-22 06:16:18', '2024-01-22 06:16:18'),
+	(11, 6, 'N-00005', 'NBI', 0, '2024-01-22 06:16:54', '2024-01-22 06:16:54'),
+	(12, 6, 'N-00005', 'Police', 0, '2024-01-22 06:16:54', '2024-01-22 06:16:54');
 
 -- Dumping structure for table qcpl_engagement.service
 CREATE TABLE IF NOT EXISTS `service` (
@@ -143,7 +214,7 @@ CREATE TABLE IF NOT EXISTS `service` (
   PRIMARY KEY (`service_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table qcpl_engagement.service: ~0 rows (approximately)
 
 -- Dumping structure for table qcpl_engagement.users
 CREATE TABLE IF NOT EXISTS `users` (
@@ -161,7 +232,10 @@ CREATE TABLE IF NOT EXISTS `users` (
   CONSTRAINT `users_ibfk_1` FOREIGN KEY (`user_role_id`) REFERENCES `user_role` (`user_role_id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table qcpl_engagement.users: ~2 rows (approximately)
+INSERT INTO `users` (`user_id`, `user_role_id`, `username`, `password`, `is_active`, `last_login`, `created_at`, `updated_at`) VALUES
+	(4, 1, 'admin', '$2y$10$MRaoOOnRQ69KTufCOTKo3u87J0E41e3XIIvYSr/3vG58uJ6NDtWWS', 1, '0000-00-00 00:00:00', '2023-12-28 02:52:00', '2023-12-28 02:52:00'),
+	(5, 2, 'staff', '$2y$10$pk3hTpWf00wNWmTR6XtSY.mOzNX3T8Gy.tmlArEtxEY3djTzoCnE6', 1, '0000-00-00 00:00:00', '2023-12-28 02:53:48', '2023-12-28 02:53:48');
 
 -- Dumping structure for table qcpl_engagement.user_role
 CREATE TABLE IF NOT EXISTS `user_role` (
@@ -172,7 +246,10 @@ CREATE TABLE IF NOT EXISTS `user_role` (
   PRIMARY KEY (`user_role_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table qcpl_engagement.user_role: ~2 rows (approximately)
+INSERT INTO `user_role` (`user_role_id`, `user_role`, `created_at`, `updated_at`) VALUES
+	(1, 'Admin', '2023-12-06 14:57:56', '2023-12-06 15:01:56'),
+	(2, 'Staff', '2023-12-06 14:58:17', '2023-12-06 15:02:06');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
