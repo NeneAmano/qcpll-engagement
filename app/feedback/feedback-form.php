65756b770e6600ae082361a7e086ab75a3d3c7ca
n67h <night67hunter@gmail.com>
2024-01-23 10:51:57 +0800

    Change the currentTab from 0 to 1

--- app/feedback/feedback-form.php
+++ app/feedback/feedback-form.php
@@ -247,15 +247,16 @@ if(isset($_POST['submit'])){
         </div>
     </form>
     <script>
-        var currentTab = 0; // Current tab is set to be the first tab (0)
+        var currentTab = 1; // Current tab is set to be the first tab (0)
         showTab(currentTab); // Display the current tab
 
         function showTab(n) {
+            console.log("Showing tab " + n);
             // This function will display the specified tab of the form...
             var x = document.getElementsByClassName("tab");
             x[n].style.display = "block";
             //... and fix the Previous/Next buttons:
-            if (n == 0) {
+            if (n == 1) {
                 document.getElementById("prevBtn").style.display = "none";
             } else {
                 document.getElementById("prevBtn").style.display = "inline";
@@ -273,6 +274,7 @@ if(isset($_POST['submit'])){
         }
 
         function nextPrev(n) {
+            console.log("Current tab: " + currentTab);
             // This function will figure out which tab to display
             var x = document.getElementsByClassName("tab");
             // Exit the function if any field in the current tab is invalid:
@@ -282,7 +284,7 @@ if(isset($_POST['submit'])){
             // Increase or decrease the current tab by 1:
             currentTab += n;
             // if you have reached the end of the form...
-            if (currentTab >= x.length) {
+            if (currentTab > x.length) {
                 // ... the form gets submitted:
                 // document.getElementById("regForm").submit();
                 return false;
