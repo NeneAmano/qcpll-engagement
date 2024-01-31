ca7ce31a8e7031b85f956d0dd0ace347ad12bd75
n67h <night67hunter@gmail.com>
2024-01-31 15:40:57 +0800

    Require_once feedback header

--- app/feedback/feedback-form.php
+++ app/feedback/feedback-form.php
@@ -1,281 +1,105 @@
 <?php
-    require_once('../core/init.php');
-    if(isset($_GET['client_id'])){
-        $client_id = $_GET['client_id'];
-        $sql = "UPDATE queue_details SET status = 1 WHERE client_id = $client_id;";
-        mysqli_query($conn, $sql);
-    }else{
-        header('location:feedback.php');
-        die();
-    }
+    require_once 'includes/feedback-header.php';
 ?>
-<!DOCTYPE html>
-<html>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Feedback</title>
-<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
-<!-- latest bootstrap cdn -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-<!-- jquery ajax -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-</head>
-<style>
-    * {
-        box-sizing: border-box;
-    }
-    body {
-        background-color: #f1f1f1;
-    }
-    #regForm {
-        background-color: #ffffff;
-        margin: 100px auto;
-        font-family: Raleway;
-        padding: 40px;
-        width: 70%;
-        min-width: 300px;
-    }
-    h1 {
-        text-align: center;
-    }
-    input {
-        padding: 10px;
-        width: 100%;
-        font-size: 17px;
-        font-family: Raleway;
-        border: 1px solid #aaaaaa;
-    }
-    /* Mark input boxes that gets an error on validation: */
-    input.invalid {
-        background-color: #ffdddd;
-    }
-    /* Hide all steps by default: */
-    .tab {
-        display: none;
-    }
-    button {
-        background-color: #04AA6D;
-        color: #ffffff;
-        border: none;
-        padding: 10px 20px;
-        font-size: 17px;
-        font-family: Raleway;
-        cursor: pointer;
-    }
-    button:hover {
-        opacity: 0.8;
-    }
-    #prevBtn {
-        background-color: #bbbbbb;
-    }
-    /* Make circles that indicate the steps of the form: */
-    .step {
-        height: 15px;
-        width: 15px;
-        margin: 0 2px;
-        background-color: #bbbbbb;
-        border: none;
-        border-radius: 50%;
-        display: inline-block;
-        opacity: 0.5;
-    }
-    .step.active {
-        opacity: 1;
-    }
-    /* Mark the steps that are finished and valid: */
-    .step.finish {
-        background-color: #04AA6D;
-    }
-    body {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        height: 100vh;
-        margin: 0;
-        background-color: #f0f0f0;
-        font-family: 'Arial', sans-serif;
-    }
-    #question-container {
-        text-align: center;
-        padding: 30px;
-        background-color: #fff;
-        border-radius: 10px;
-        box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
-    }
-    #question {
-        font-size: 1.8em;
-        margin-bottom: 20px;
-    }
-    .reaction-container {
-        display: flex;
-        justify-content: space-around;
-        gap: 0.5em;
-    }
-    .reaction {
-        text-align: center;
-    }
-    .reaction img {
-        width: 100px;
-        /* Adjust the size as needed */
-        margin-top: 10px;
-    }
-    .reaction img:hover {
-        transform: translate(0px, -20px);
-        transition: ease-in 0.1s;
-        cursor: pointer;
-    }
-    .word {
-        font-size: 1.2em;
-        margin-top: 10px;
-    }
-    label {
-        font-size: 1em;
-    }
-
-    /* HIDE RADIO */
-[type=radio] { 
-  position: absolute;
-  opacity: 0;
-  width: 0;
-  height: 0;
-}
-
-/* IMAGE STYLES */
-[type=radio] + img {
-  cursor: pointer;
-}
-
-/* CHECKED STYLES */
-[type=radio]:checked + img {
-  transform: translate(0px, -18px);
-  box-shadow: 26px 200px 203px -96px rgba(0,0,0,1);
--webkit-box-shadow: 26px 200px 203px -96px rgba(0,0,0,1);
--moz-box-shadow: 26px 200px 203px -96px rgba(0,0,0,1);
-  filter: drop-shadow(5px 5px 5px rgba(0,0,0,0.3));
-  border-radius: 50%;
-  background-color: transparent;
-}
-.hidden {
-    display: none;
-}
-</style>
 <body>
     <form id="regForm" method="post" action="">
         <!-- One "tab" for each step in the form: -->
         <?php
-            // get the total rows of question table
-            $sql_row = "SELECT COUNT(question_id) AS total_rows FROM questions;";
-            $result_row = mysqli_query($conn, $sql_row);
-            if(mysqli_num_rows($result_row) > 0){
-                while($row_row = mysqli_fetch_assoc($result_row)){
-                    $total_rows = $row_row['total_rows'];
-                }
+        // get the total rows of question table
+        $sql_row = "SELECT COUNT(question_id) AS total_rows FROM questions;";
+        $result_row = mysqli_query($conn, $sql_row);
+        if (mysqli_num_rows($result_row) > 0) {
+            while ($row_row = mysqli_fetch_assoc($result_row)) {
+                $total_rows = $row_row['total_rows'];
             }
-            if(isset($_POST['submit'])){
-                // $username = mysqli_real_escape_string($conn, $_POST['username']);
-                // $username = mysqli_real_escape_string($conn, $_POST['username']);
-                // Iterate through $others and echo the values
+        }
+        if (isset($_POST['submit'])) {
+            // $username = mysqli_real_escape_string($conn, $_POST['username']);
+            // $username = mysqli_real_escape_string($conn, $_POST['username']);
+            // Iterate through $others and echo the values
+            for ($i = 1; $i <= $total_rows; $i++) {
+                if (isset($_POST["question{$i}"])) {
+                    $question_value = mysqli_real_escape_string($conn, $_POST["question{$i}"]);
 
-                
-                
-
-                for ($i = 1; $i <= $total_rows; $i++) {
-                    // Check if the emoji radio button for the current question exists in $_POST
-                    // if (isset($_POST["emoji{$i}"])) {
-                    //     // Get the value of the emoji radio button for the current question
-                    //     $emoji_value = mysqli_real_escape_string($conn, $_POST["emoji{$i}"]);
-                    //     echo $emoji_value;
-                    // }
-
-                    // if (isset($_POST["text{$i}"])) {
-                    //     // Get the value of the emoji radio button for the current question
-                    //     $text_value = mysqli_real_escape_string($conn, $_POST["text{$i}"]);
-                    //     echo $text_value;
-                    // }
-
-                    if(isset($_POST["question{$i}"])){
-                        $question_value = mysqli_real_escape_string($conn, $_POST["question{$i}"]);
-
-                        $sql_qt = "SELECT qt_id FROM questions WHERE question_id = $question_value;";
-                        $result_qt = mysqli_query($conn, $sql_qt);
-                        if(mysqli_num_rows($result_qt) > 0){
-                            $row_qt = mysqli_fetch_assoc($result_qt);
-                            $question_type = $row_qt['qt_id'];
-                        }
-
-                        if($question_type == 2){
-                            $emoji_value = 6;
-                        }
-                        
-                        if (isset($_POST["text{$i}"])) {
-                            // Get the value of the emoji radio button for the current question
-                            $text_value = mysqli_real_escape_string($conn, $_POST["text{$i}"]);
-                            echo $text_value;
-                            
-                        }
-
-                        if (isset($_POST["emoji{$i}"])) {
-                            // Get the value of the emoji radio button for the current question
-                            $emoji_value = mysqli_real_escape_string($conn, $_POST["emoji{$i}"]);
-                            echo $emoji_value;
-                        }
-
-                        $sql = "INSERT INTO feedback (client_id, question_id, emoji_id, text_feedback) VALUES ($client_id, $question_value, $emoji_value, '$text_value');";
-                        mysqli_query($conn, $sql);
+                    $sql_qt = "SELECT qt_id FROM questions WHERE question_id = $question_value;";
+                    $result_qt = mysqli_query($conn, $sql_qt);
+                    if (mysqli_num_rows($result_qt) > 0) {
+                        $row_qt = mysqli_fetch_assoc($result_qt);
+                        $question_type = $row_qt['qt_id'];
                     }
+
+                    if ($question_type == 2) {
+                        $emoji_value = 6;
+                    }
+
+                    if (isset($_POST["text{$i}"])) {
+                        // Get the value of the emoji radio button for the current question
+                        $text_value = mysqli_real_escape_string($conn, $_POST["text{$i}"]);
+                        echo $text_value;
+                    }
+
+                    if (isset($_POST["emoji{$i}"])) {
+                        // Get the value of the emoji radio button for the current question
+                        $emoji_value = mysqli_real_escape_string($conn, $_POST["emoji{$i}"]);
+                        echo $emoji_value;
+                    }
+
+                    $sql = "INSERT INTO feedback (client_id, question_id, emoji_id, text_feedback) VALUES ($client_id, $question_value, $emoji_value, '$text_value');";
+                    mysqli_query($conn, $sql);
                 }
             }
-            $sql = "SELECT * FROM questions WHERE is_deleted != 1;";
-            $result = mysqli_query($conn, $sql);
-            if(mysqli_num_rows($result) > 0) {
-                while($row = mysqli_fetch_assoc($result)){
-                    $question_id = $row['question_id'];
-                    $qt_id = $row['qt_id'];
-                    $english_question = $row['english_question'];
-                    $tagalog_question = $row['tagalog_question'];
+        }
+        $sql = "SELECT * FROM questions WHERE is_deleted != 1;";
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $question_id = $row['question_id'];
+                $qt_id = $row['qt_id'];
+                $english_question = $row['english_question'];
+                $tagalog_question = $row['tagalog_question'];
         ?>
-                    <!-- emoji based answer starts here -->
-                    <div class="tab">
-                        <div id="question-container">
-                            <p id="english<?= $question_id ?>" class="fs-4"><?= $english_question; ?></p>
-                            <p id="tagalog<?= $question_id ?>" class="fs-6 fst-italic"><?= $tagalog_question; ?></p>
-                            <input type="hidden" value="<?= $question_id ?>" name="question<?= $question_id ?>" id="answer<?= $question_id ?>">
-                            <div class="reaction-container">
+                <!-- emoji based answer starts here -->
+                <div class="tab">
+                    <div id="question-container">
+                        <p id="english<?= $question_id ?>" class="fs-4"><?= $english_question; ?></p>
+                        <p id="tagalog<?= $question_id ?>" class="fs-6 fst-italic"><?= $tagalog_question; ?></p>
+                        <input type="hidden" value="<?= $question_id ?>" name="question<?= $question_id ?>" id="answer<?= $question_id ?>">
+                        <div class="reaction-container">
+                            <?php
+                            if ($qt_id == 1) {
+                                $sql_emoji = "SELECT * FROM emoji WHERE UNICODE != '0' ORDER BY emoji_id DESC LIMIT 5;";
+                                $result_emoji = mysqli_query($conn, $sql_emoji);
+                                if (mysqli_num_rows($result_emoji) > 0) {
+                                    while ($row_emoji = mysqli_fetch_assoc($result_emoji)) {
+                                        $emoji_id = $row_emoji['emoji_id'];
+                                        $image_path = $row_emoji['image_path'];
+                                        $value = $row_emoji['value'];
+                            ?>
+                                        <div class="reaction">
+                                            <label>
+                                                <input type="radio" id="emoji<?= $question_id . $emoji_id ?>" name="emoji<?= $question_id ?>" value="<?= $emoji_id ?>" required>
+                                                <img src="../../<?= $image_path ?>" alt="Option 1">
+                                            </label>
+                                        </div>
                                 <?php
-                                    if($qt_id == 1){
-                                        $sql_emoji = "SELECT * FROM emoji WHERE UNICODE != '0' ORDER BY emoji_id DESC LIMIT 5;";
-                                        $result_emoji = mysqli_query($conn, $sql_emoji);
-                                        if(mysqli_num_rows($result_emoji) > 0){
-                                            while($row_emoji = mysqli_fetch_assoc($result_emoji)){
-                                                $emoji_id = $row_emoji['emoji_id'];
-                                                $image_path = $row_emoji['image_path'];
-                                                $value = $row_emoji['value'];
-                                        ?>
-                                                <div class="reaction">
-                                                <label>
-                                                <input type="radio" id="emoji<?=$question_id .$emoji_id?>" name="emoji<?= $question_id ?>" value="<?=$emoji_id?>" required>
-                                                <img src="../../<?= $image_path?>" alt="Option 1">
-                                                </label>
-                                                </div>
-                                        <?php
-                                            }
-                                        }
-                                    }else{
-                                    ?>
-                                        <input class="form-control form-control-lg" type="text" placeholder="" name="text<?= $question_id ?>" aria-label=".form-control-lg example" value="">
-                                    <?php
                                     }
+                                }
+                            } else {
                                 ?>
-                            </div>
+                                <input class="form-control form-control-lg" type="text" placeholder="" name="text<?= $question_id ?>" aria-label=".form-control-lg example" value="">
+                            <?php
+                            }
+                            ?>
                         </div>
                     </div>
+                </div>
         <?php
-                }
             }
+        }
         ?>
-  
         <!-- emoji based answer ends here -->
         <div style="overflow:auto;">
-        
+
             <div style="float:right; margin-top:12px;">
                 <button type="button" id="prevBtn" onclick="nextPrev(-1)">Previous</button>
                 <button type="button" id="nextBtn" onclick="nextPrev(1)" class="">Next</button>
@@ -285,9 +109,9 @@
         <!-- Circles which indicates the steps of the form: -->
         <div style="text-align:center;margin-top:40px;">
             <?php
-                for ($i = 0; $i < $total_rows; $i++) {
-                    echo '<span class="step"></span>';
-                }
+            for ($i = 0; $i < $total_rows; $i++) {
+                echo '<span class="step"></span>';
+            }
             ?>
         </div>
     </form>
@@ -375,5 +199,4 @@
     <!-- latest bootstrap js -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
 </body>
-
 </html>
