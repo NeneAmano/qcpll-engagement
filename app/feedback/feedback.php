44bbaa905a5d289979d9cf4e2969c982deb68536
n67h <night67hunter@gmail.com>
2024-01-31 15:39:07 +0800

    Change title

--- app/feedback/feedback.php
+++ app/feedback/feedback.php
@@ -6,14 +6,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Quezon City Public Library Queue</title>
+    <title>Quezon City Public Library</title>
     <link rel="stylesheet" href="../../public/assets/css/style.css">
     <link rel="shortcut icon" href="../../public/assets/images/qcplLogo.png" type="image/x-icon">
     <link href="https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-
 <section id="swup" class="transtion-fade">
     <div class="logo">
         <img src="./images/qclogo.jpg" alt="">
