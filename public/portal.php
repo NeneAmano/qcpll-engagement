74e807d262b14aa21250b1afb7d2c1771d8ad1e2
n67h <night67hunter@gmail.com>
2024-01-31 15:53:21 +0800

    Reformat file

--- public/portal.php
+++ public/portal.php
@@ -1,7 +1,6 @@
 <?php 
     require_once("../app/includes/header.php");
 ?>
-
 <body>
 <style>
     .dropdown{
@@ -13,54 +12,51 @@
     .dropdown:hover>.dropdown-menu {
         display: block;
 }
-
 .dropdown>.dropdown-toggle:active {
   /*Without this, clicking will make it sticky*/
     pointer-events: none;
 }
 </style>
     <section id="swup" class="transtion-fade">
-    <?php  if(isset($_SESSION['user_id'])){
-        $sql = "SELECT username FROM users WHERE user_id = $user_id_session";
-        $res = mysqli_query($conn,$sql);
-        while($row = mysqli_fetch_assoc($res)){
-            $username = $row['username'];
-            echo '<div class="dropdown">';
-                echo '<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                    '.$username.'
-                </button>';
-                    echo '<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">';
-                        echo '<li><a class="dropdown-item" href="../app/includes/logout.php">Log out</a></li>';
-                    echo '</ul>';
-            echo '</div>';
+    <?php  
+        if(isset($_SESSION['user_id'])){
+            $sql = "SELECT username FROM users WHERE user_id = $user_id_session";
+            $res = mysqli_query($conn,$sql);
+            while($row = mysqli_fetch_assoc($res)){
+                $username = $row['username'];
+                echo '<div class="dropdown">';
+                    echo '<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                        '.$username.'
+                    </button>';
+                        echo '<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">';
+                            echo '<li><a class="dropdown-item" href="../app/includes/logout.php">Log out</a></li>';
+                        echo '</ul>';
+                echo '</div>';
+            }
         }
-       }?>
+    ?>
     <div class="logo">
-            <img src="./images/qclogo.jpg" alt="">
-            <div class="title">
+        <img src="./images/qclogo.jpg" alt="">
+        <div class="title">
             <p>Quezon City Public Library</p>
             <p>Quezon City Government</p>
-            </div>
-            <img src="./images/qcplLogo.png" alt="">
         </div>
-        <div class="container-1">
-            <div class="promotext-1">
-                <h1>Welcome to </h1>
-                <h3>EngageMate</h3>
-                <p>Offers free assistance to all individuals <br>
-                 who wish to access the online government services <br>
-                 offered by different department agencies of the Philippines.</p>
-            </div>
-            <div class="pathBtn">
-                <button class="QueueBtn" onclick="window.location.href='../app/queuing/queue.php';">Queuing</button>
-               <button class="FeedbackBtn" onclick="window.location.href='../app/feedback/feedback.php';">Feedback</button>
-               <!-- <a href="feedback.php" class="FeedbackBtn">To Feedback</a> -->
-            </div>
+        <img src="./images/qcplLogo.png" alt="">
+    </div>
+    <div class="container-1">
+        <div class="promotext-1">
+            <h1>Welcome to </h1>
+            <h3>EngageMate</h3>
+            <p>Offers free assistance to all individuals <br>
+                who wish to access the online government services <br>
+                offered by different department agencies of the Philippines.</p>
         </div>
-        
-
-    
-
+        <div class="pathBtn">
+            <button class="QueueBtn" onclick="window.location.href='../app/queuing/queue.php';">Queuing</button>
+            <button class="FeedbackBtn" onclick="window.location.href='../app/feedback/feedback.php';">Feedback</button>
+            <!-- <a href="feedback.php" class="FeedbackBtn">To Feedback</a> -->
+        </div>
+    </div>
     <script src="https://unpkg.com/swup@4"></script>
     <script src="script.js"></script>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
