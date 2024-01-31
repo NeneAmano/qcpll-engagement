239a75fbdc3a892d33cc74af17d863ef49a48eaa
n67h <night67hunter@gmail.com>
2024-01-31 17:09:17 +0800

    WIP  - html elements

---
+++ app/queuing/police-clearance-form.php
@@ -0,0 +1,46 @@
+<?php
+    require_once('../core/init.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Police Clearance Form</title>
+    <link rel="shortcut icon" href="../../public/assets/images/qcplLogo.png" type="image/x-icon">
+    <link href="http://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css" rel="stylesheet">
+    <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://kit.fontawesome.com/3481525a72.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+    <div class="main-container mt-2">
+        <h1 class="text-center text-dark">Police Clearance Form</h1>
+        <div class="row">
+            <div class="col-md-12 bg-warning">
+                <h3 class="text-dark text-center">Applicant Profile</h3>
+                <div class="container-md border border-dark p-5">
+                    
+                    <p>Valid/working e-mail address <span class="fs-6">(ex. juandelacruz@gmail.com):</span>
+                    <br>
+                    <u>________________________________________________________________</u></p>
+                    <p>Unang pangalan:<u>____________________________________________</u></p>
+                    <p>Gitnang pangalan:<u>____________________________________________</u></p>
+                    <p>Apelyido:<u>____________________________________________</u></p>
+                    <p><u>____________________________________________</u></p>
+                    <p><u>____________________________________________</u></p>
+                </div>
+                <div class="container-md">
+                    <h3 class="text-dark text-center">Other Information</h3>
+                    <p>Valid/working e-mail address <span class="fs-6">(ex. juandelacruz@gmail.com):</span><u>____________________________________________</u></p>
+                </div>
+            </div>
+            <div class="col-md-6 bg">
+                
+            </div>
+        </div>
+    </div>
+</body>
+</html>
