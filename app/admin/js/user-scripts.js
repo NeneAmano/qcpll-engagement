4b4a0fcd02349d1c889c569bf9cd5a00864ea06f
n67h <night67hunter@gmail.com>
2024-02-04 16:06:36 +0800

    Fix the edit_user_password nasty af bug

--- app/admin/js/user-scripts.js
+++ app/admin/js/user-scripts.js
@@ -1,36 +1,22 @@
 $(document).ready(function () {
-    // $('body').on('click', '.edit', function(event) {
-    $('body').on('click', '.edit', function(event) {
-
-        $('#edit_user_modal').modal('show');
-
-        $tr = $(this).closest('tr');
-
+    $('body').on('click', '.edit', function (event) {
+        var $tr = $(this).closest('tr');
         var data = $tr.children("td").map(function () {
             return $(this).text();
         }).get();
 
-        // console.log(data);
+        // Check if the modal type is 'password' or 'user'
+        var modalType = $(this).data('modal-type');
 
         $('#edit_user_role').val(data[0]).change();
-        $('#edit_user_id').val(data[1])
+        $('#edit_user_id').val(data[1]);
         $('#edit_username').val(data[3]);
-    });
-});
+        $('#edit_user_password_id').val(data[1]);
 
-$(document).ready(function () {
-    $('body').on('click', '#edit', function(event) {
-
-        $('#edit_user_password_modal').modal('show');
-
-        $tr = $(this).closest('tr');
-
-        var data = $tr.children("td").map(function () {
-            return $(this).text();
-        }).get();
-
-        console.log(data);
-
-        $('#edit_user_password_id').val(data[1])
+        if (modalType === 'user') {
+            $('#edit_user_modal').modal('show');
+        } else if(modalType === 'password') {
+            $('#edit_user_password_modal').modal('show');
+        }
     });
 });
