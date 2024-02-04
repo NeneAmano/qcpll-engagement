a4d6f374ca0be042f4a7deec87fc52a4c6a0a0b4
n67h <night67hunter@gmail.com>
2024-02-04 16:40:31 +0800

    WIP  - change password

---
+++ app/admin/functions/edit-user-password.php
@@ -0,0 +1,33 @@
+<?php
+    require_once '../../core/init.php';
+
+    if(isset($_POST['edit_user_password'])){
+        $edit_user_password_id = mysqli_real_escape_string($conn, $_POST['edit_user_password_id']);
+        $edit_user_role = mysqli_real_escape_string($conn, $_POST['edit_user_role']);
+        $edit_username = mysqli_real_escape_string($conn, $_POST['edit_username']);
+
+        if(empty($edit_user_id) || empty($edit_user_role) || empty($edit_username)){
+            $error_message = "All fields are required!";
+            echo "<script type='text/javascript'>alert('$error_message');</script>";
+            $error_redirect = '<h3 style="color: red; text-align: center;">All fields are required! You will be redirected to previous page in <span id="counter">5</span> second(s).</h3>
+            <script type="text/javascript">
+                function countdown() {
+                    var i = document.getElementById("counter");
+                    if (parseInt(i.innerHTML)<=0) {
+                        location.href = "../users.php";
+                    }
+                    i.innerHTML = parseInt(i.innerHTML)-1;
+                }
+                setInterval(function(){ countdown(); },1000);
+            </script>';
+            echo $error_redirect;
+            header("refresh:5;url=../users.php");
+            die();
+        }else{
+            $sql = "UPDATE users SET user_role_id = $edit_user_role, username = '$edit_username' WHERE user_id = $edit_user_id;";
+            if(mysqli_query($conn, $sql)){
+                header("location: ../users.php?user=successfullyedited");
+                die();
+            }
+        }
+    }
