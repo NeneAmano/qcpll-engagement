6cd7dcb554916ad51255e404ba6036143e07bb25
BERT151 <102898303+BERT151@users.noreply.github.com>
2024-02-04 16:05:58 +0800

    Update feedback-form.php

--- app/feedback/feedback-form.php
+++ app/feedback/feedback-form.php
@@ -115,8 +115,8 @@
             margin-top: 10px;
         }
         .reaction img:hover {
-            transform: translate(0px, -20px);
-            transition: ease-in 0.1s;
+            width: 120px;
+            transition: ease-in-out 0.2s;
             cursor: pointer;
         }
         .word {
@@ -142,7 +142,7 @@
 
     /* CHECKED STYLES */
     [type=radio]:checked + img {
-    transform: translate(0px, -18px);
+        width: 120px;
     box-shadow: 26px 200px 203px -96px rgba(0,0,0,1);
     -webkit-box-shadow: 26px 200px 203px -96px rgba(0,0,0,1);
     -moz-box-shadow: 26px 200px 203px -96px rgba(0,0,0,1);
@@ -155,7 +155,7 @@
     }
     
     /* for the next page btn design */
-  button {
+  button,.btn-lg {
   cursor: pointer;
   font-weight: 700;
   font-family: Helvetica,"sans-serif";
@@ -170,21 +170,21 @@
   
 }
 
-button:hover {
+button:hover,.btn-lg:hover {
   background: #68c4af;
 }
 
-button > svg {
+button > svg,.btn-lg > svg {
   width: 34px;
   margin-left: 10px;
   transition: transform .3s ease-in-out;
 }
 
-button:hover svg {
+button:hover svg,.btn-lg:hover svg {
   transform: translateX(5px);
 }
 
-button:active {
+button:active,.btn-lg:active {
   transform: scale(0.95);
 }
 
@@ -312,7 +312,13 @@ button:active {
                 </svg>
             </button>
 
-                <button type="submit" class="d-none" id="submit" name="submit">Submit</button>
+                <button type="submit" class="d-none" id="submit" name="submit">
+                <span>Submit</span>
+                <svg width="34" height="34" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="37" cy="37" r="35.5" stroke="black" stroke-width="3"></circle>
+                    <path d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284 24.1716 38.5 25 38.5V35.5ZM49.0607 38.0607C49.6464 37.4749 49.6464 36.5251 49.0607 35.9393L39.5147 26.3934C38.9289 25.8076 37.9792 25.8076 37.3934 26.3934C36.8076 26.9792 36.8076 27.9289 37.3934 28.5147L45.8787 37L37.3934 45.4853C36.8076 46.0711 36.8076 47.0208 37.3934 47.6066C37.9792 48.1924 38.9289 48.1924 39.5147 47.6066L49.0607 38.0607ZM25 38.5L48 38.5V35.5L25 35.5V38.5Z" fill="black"></path>
+                </svg>
+                </button>
             </div>
         </div>
 
