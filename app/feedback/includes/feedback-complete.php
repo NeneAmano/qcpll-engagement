cf87d6d90c5a6b4c46939f708ea2aa8eeb15668c
n67h <night67hunter@gmail.com>
2024-02-07 12:46:49 +0800

    Add function-complete.php

---
+++ app/feedback/includes/feedback-complete.php
@@ -0,0 +1,14 @@
+<?php
+    require_once('../../core/init.php');
+    if(isset($_GET['client_id'])){
+        $client_id = $_GET['client_id'];
+
+        $sql = "UPDATE queue_details SET status = 1 WHERE client_id = $client_id;";
+        if(mysqli_query($conn, $sql)){
+            header('location: ../feedback.php');
+            die();
+        }
+    }else{
+        header('location: ../feedback.php');
+        die();
+    }
