09d02973a6fe940360a664e170cd8fb4b632d8ab
BERT151 <102898303+BERT151@users.noreply.github.com>
2024-02-09 20:43:16 +0800

    -add function to update status

--- app/feedback/feedback.php
+++ app/feedback/feedback.php
@@ -1,12 +1,16 @@
 <?php
-    require_once('../core/init.php');
-    if(!isset($_SESSION['user_id'])){
-        header('location: ../../public/index.php');
-        die();
-    }
+require_once('../core/init.php');
+if (!isset($_SESSION['user_id'])) {
+    header('location: ../../public/index.php');
+    die();
+}
+
+// Check if client_id is set
+$client_id = isset($_POST['id']) ? $_POST['id'] : null;
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -24,215 +28,216 @@
         justify-content: flex-start;
     }
 
-    .dropdown:hover > .dropdown-menu {
+    .dropdown:hover>.dropdown-menu {
         display: block;
     }
 
-    .dropdown > .dropdown-toggle:active {
+    .dropdown>.dropdown-toggle:active {
         pointer-events: none;
     }
 
+    .m-b {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        color: #212121;
+    }
 
-
+    .check-icon {
+        background-color: #212121;
+        padding: 30px;
+        height: 200px !important;
+        border-radius: 50%;
+    }
 </style>
+
 <body>
-<section id="swup" class="transtion-fade">
-    
+    <section id="swup" class="transtion-fade">
+
         <div class="logo">
             <img src="../../public/assets/images/qclogo.jpg" alt="">
             <div class="title">
-            <p>Quezon City Public Library</p>
-            <p>Quezon City Government</p>
+                <p>Quezon City Public Library</p>
+                <p>Quezon City Government</p>
             </div>
             <img src="../../public/assets/images/qcplLogo.png" alt="">
         </div>
-
-
-
         <br>
         <br>
         <br>
         <br>
 
-    <!-- Start of feedback form -->
-    <form action="" method="post">
-        <div class="flex">
-            <div class="textBox">
-                <p style="font-size: 1.5em;">Please enter your queue number</p>
-                <p style="font-size: 1em; font-style:italic;">Mangyaring ilagay sa ibaba inyong queue number</p>
-                <input type="text" name="id" id="" class="inputnum">
-                <br>
-                <br>
-                <button class="btn btn-success flex" type="submit" name="search">Search my Name</button>
-                <?php
-                    if(isset($_POST['search'])){
+        <!-- Start of feedback form -->
+        <form action="" method="post">
+            <div class="flex">
+                <div class="textBox">
+                    <p style="font-size: 1.5em;">Please enter your queue number</p>
+                    <p style="font-size: 1em; font-style:italic;">Mangyaring ilagay sa ibaba inyong queue number</p>
+                    <input type="text" name="id" id="" class="inputnum">
+                    <br>
+                    <br>
+                    <button class="btn btn-success flex" type="submit" name="search">Search my Name</button>
+                    <?php
+                    if (isset($_POST['search'])) {
                         $id = $_POST['id'];
 
                         $sql = "SELECT CONCAT(LEFT(client.f_name, 1), REPEAT('*', LENGTH(client.f_name) - 1), ' ', LEFT(client.l_name, 1), REPEAT('*', LENGTH(client.l_name) - 1)) AS masked_name, queue_details.queue_number, queue_details.client_id, queue_details.created_at AS created_at FROM client INNER JOIN queue_details ON client.client_id = queue_details.client_id WHERE queue_number = '$id' AND DATE(queue_details.created_at) = CURDATE() AND queue_details.entry_check = 1 LIMIT 1";
-                        $res = mysqli_query($conn,$sql);
+                        $res = mysqli_query($conn, $sql);
 
-                        if($row = mysqli_fetch_array($res)){
+                        if ($row = mysqli_fetch_array($res)) {
                             $client_id = $row['client_id']
-                            ?>
-                <p style="font-size: 1.5em;">Client Identifier</p>
-                <p style="font-size: 1em; font-style:italic;">Note* This is a type box that will auto-fill when your account exists.</p>
-                
-                <p style="font-size: 1em; font-style:italic;">Paalala* Ito ay kusang naglalagay ng pangalan kung kayo ay nakagawa na ng account.</p>
-                <input type="text" name="" id="" disabled class="inputnum" style="padding:0px;" value="<?php echo $row['masked_name']?>"/>
+                    ?>
+                            <p style="font-size: 1.5em;">Client Identifier</p>
+                            <p style="font-size: 1em; font-style:italic;">Note* This is a type box that will auto-fill when your account exists.</p>
 
-                <button type="button" class="btn btn-success feedbackbtn" data-toggle="modal" data-target="#exampleModalCenter">Submit</button>
+                            <p style="font-size: 1em; font-style:italic;">Paalala* Ito ay kusang naglalagay ng pangalan kung kayo ay nakagawa na ng account.</p>
+                            <input type="text" name="" id="" disabled class="inputnum" style="padding:0px;" value="<?php echo $row['masked_name'] ?>" />
+
+                            <button type="button" class="btn btn-success feedbackbtn" data-toggle="modal" data-target="#exampleModalCenter">Submit</button>
                     <?php
-            }else
-            {
-                echo '<br><br>';
-                echo '<div class="alert alert-danger" role="alert">';
-                echo 'No Data was found! Please enter again!';
-                echo '</div>';
-            }
+                        } else {
+                            echo '<br><br>';
+                            echo '<div class="alert alert-danger" role="alert">';
+                            echo 'No Data was found! Please enter again!';
+                            echo '</div>';
+                        }
                     }
-                ?>
-                <br>
-                <br>
-                <br>
-            </div>
-            
-            <div class="numPan">
-                <div class="nums">
-                    <div class="flex r r1">
-                        <div><span>1</span></div>
-                        <div><span>2</span></div>
-                        <div><span>3</span></div>
-                    </div>
-                    <div class="flex r r2">
-                        <div><span>4</span></div>
-                        <div><span>5</span></div>
-                        <div><span>6</span></div>
-                    </div>
-                    <div class="flex r r3">
-                        <div><span>7</span></div>
-                        <div><span>8</span></div>
-                        <div><span>9</span></div>
-                    </div>
-                    <div class="flex r r4">
-                        <div><span>-</span></div>
-                        <div><span>0</span></div>
-                        <div class="del"><span>Del</span></div>
-                    </div>
-                    <div class="flex r r5">
-                        <div><span>N</span></div>
-                        <div class=""></div>
-                        <div><span>P</span></div>
+                    ?>
+                    <br>
+                    <br>
+                    <br>
+                </div>
+
+                <div class="numPan">
+                    <div class="nums">
+                        <div class="flex r r1">
+                            <div><span>1</span></div>
+                            <div><span>2</span></div>
+                            <div><span>3</span></div>
+                        </div>
+                        <div class="flex r r2">
+                            <div><span>4</span></div>
+                            <div><span>5</span></div>
+                            <div><span>6</span></div>
+                        </div>
+                        <div class="flex r r3">
+                            <div><span>7</span></div>
+                            <div><span>8</span></div>
+                            <div><span>9</span></div>
+                        </div>
+                        <div class="flex r r4">
+                            <div><span>-</span></div>
+                            <div><span>0</span></div>
+                            <div class="del"><span>Del</span></div>
+                        </div>
+                        <div class="flex r r5">
+                            <div><span>N</span></div>
+                            <div class=""></div>
+                            <div><span>P</span></div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </form>
-</section>
+        </form>
+    </section>
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-            <?php
-                if(isset($_POST['search'])){
-                    $id = $_POST['id'];
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <?php
+                    if (isset($_POST['search'])) {
+                        $id = $_POST['id'];
 
-                    $sql = "SELECT CONCAT(LEFT(client.f_name, 1), REPEAT('*', LENGTH(client.f_name) - 1), ' ', LEFT(client.l_name, 1), REPEAT('*', LENGTH(client.l_name) - 1)) AS masked_name, queue_details.queue_number FROM client INNER JOIN queue_details ON client.client_id = queue_details.client_id WHERE queue_number = '$id' LIMIT 1";
-                    $res = mysqli_query($conn,$sql);
+                        $sql = "SELECT CONCAT(LEFT(client.f_name, 1), REPEAT('*', LENGTH(client.f_name) - 1), ' ', LEFT(client.l_name, 1), REPEAT('*', LENGTH(client.l_name) - 1)) AS masked_name, queue_details.queue_number FROM client INNER JOIN queue_details ON client.client_id = queue_details.client_id WHERE queue_number = '$id' LIMIT 1";
+                        $res = mysqli_query($conn, $sql);
 
-                    while($row = mysqli_fetch_array($res)){
-            ?>
-                        <p style="font-style: italic;">Hi,</p>
-                        <p><?php echo $row['masked_name']?></p>
-                <?php
+                        while ($row = mysqli_fetch_array($res)) {
+                    ?>
+                            <p style="font-style: italic;">Hi,</p>
+                            <p><?php echo $row['masked_name'] ?></p>
+                    <?php
+                        }
+                    } else {
                     }
-                }else{
-                    
-                }
-            ?>
-                <p>Would you like to submit a feedback?</p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" id="feedbackbtn" data-bs-toggle="modal" data-bs-target="#exampleModalCenter1">No</button>
-                <button type="button" class="btn btn-success"><a href="feedback-form.php?client_id=<?= $client_id ?>" style="text-decoration:none; color:#fff;">Yes</a></button>
+                    ?>
+                    <p>Would you like to submit a feedback?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" id="feedbackbtn1" data-bs-toggle="modal" data-bs-target="#exampleModalCenter1">No</button>
+                    <button type="button" class="btn btn-success"><a href="feedback-form.php?client_id=<?= $client_id ?>" style="text-decoration:none; color:#fff;">Yes</a></button>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
-<!--2nd Modal -->
-<div class="modal fade" id="exampleModalCenter1" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true" style="border-radius: 5px;">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header bg-dark">
-            </div>
-            <div class="modal-body">
+    <!--2nd Modal -->
+    <div class="modal fade" id="exampleModalCenter1" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true" style="border-radius: 5px;">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header bg-dark">
+                </div>
+                <div class="modal-body m-b">
 
-                <p style="text-align: center; font-size:50px;">Thank you</p>
-                <img src="../../public/assets/images/check-icon.gif" alt="" style="height: 30%;">
+                    <p style="text-align: center; font-size:50px;">Thank you</p>
+                    <img src="../../public/assets/images/check-icon.gif" alt="" class="check-icon">
+                </div>
             </div>
         </div>
     </div>
-</div>
-<style>
 
-</style>
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.7/dist/jquery.min.js"></script>
-<script src="https://unpkg.com/swup@4"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </style>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7/dist/jquery.min.js"></script>
+    <script src="https://unpkg.com/swup@4"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-<!-- script for ion icon -->
-<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <script>
+        $(document).ready(function() {
+            var btn = document.querySelectorAll(".r > div");
+            var inpt = document.querySelector(".inputnum");
 
-<script>
-    var btn = document.querySelectorAll(".r > div");
-    var inpt = document.querySelector(".inputnum");
+            btn.forEach(val => {
+                val.addEventListener("click", () => {
+                    if (val.innerText === "Del") {
+                        inpt.value = inpt.value.slice(0, -1); // Remove the last character
+                    } else {
+                        inpt.value += val.innerText;
+                    }
+                });
+            });
 
-    btn.forEach(val => {
-        val.addEventListener("click", () => {
-            if (val.innerText === "Del") {
-                inpt.value = inpt.value.slice(0, -1); // Remove the last character
-            } else {
-                inpt.value += val.innerText;
-            }
+
+            $('#feedbackbtn1').click(function() {
+                // Close the first modal
+                $('#exampleModalCenter').modal('hide');
+
+                // Show the second modal after a brief delay
+                setTimeout(function() {
+                    $('#exampleModalCenter1').modal('show');
+                }, 200); // Adjust the delay time as needed
+
+                // Redirect to the feedback-complete.php page after showing the second modal
+                setTimeout(function() {
+                    window.location.href = './includes/feedback-complete.php?client_id=<?= $client_id ?>';
+                }, 3000); // Adjust the delay time as needed
+            });
+
+            // Handle click event for "Yes" button in the first modal
+            $('#yesButton').click(function() {
+                // Redirect to the feedback form page
+                window.location.href = 'feedback-form.php?client_id=<?= $client_id ?>';
+            });
         });
-    });
-    $(document).ready(function(){
-    $('#feedbackbtn').click(function(){
-        // Close the first modal
-        $('#exampleModalCenter').modal('hide');
-
-        // Show the second modal after a brief delay
-        setTimeout(function() {
-            $('#exampleModalCenter1').modal('show');
-        }, 200); // Adjust the delay time as needed
-
-        // Redirect to the feedback-complete.php page after showing the second modal
-        setTimeout(function() {
-            window.location.href = './includes/feedback-complete.php?client_id=<?= $client_id ?>';
-        }, 3000); // Adjust the delay time as needed
-    });
-
-    // Handle click event for "Yes" button in the first modal
-    $('#yesButton').click(function() {
-        // Redirect to the feedback form page
-        window.location.href = 'feedback-form.php?client_id=<?= $client_id ?>';
-    });
-});
-
-
-
-
-
-</script>
+    </script>
 
 </body>
+
 </html>
