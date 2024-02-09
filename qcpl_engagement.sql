cf647874096e6ac58451e303343be19597fa5115
BERT151 <warnethalarcon15@gmail.com>
2024-02-09 11:53:29 +0800

    -new database

--- qcpl_engagement.sql
+++ qcpl_engagement.sql
@@ -53,40 +53,56 @@ CREATE TABLE IF NOT EXISTS `client` (
   PRIMARY KEY (`client_id`),
   KEY `age_id` (`age_id`),
   CONSTRAINT `client_ibfk_1` FOREIGN KEY (`age_id`) REFERENCES `age` (`age_id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table qcpl_engagement.client: ~4 rows (approximately)
+-- Dumping data for table qcpl_engagement.client: ~17 rows (approximately)
 INSERT INTO `client` (`client_id`, `f_name`, `m_name`, `l_name`, `suffix`, `age_id`, `gender`, `education`, `occupation`, `status`, `created_at`, `updated_at`) VALUES
-	(1, 'Bogart', 'Pederico', 'Pedring', NULL, 5, 'Male', 'Elementary Graduate', 'Unemployed', 0, '2024-01-22 05:03:51', '2024-01-22 05:03:57'),
-	(4, 'asdasdasd', '', 'asdsadasd', '', 5, 'Female', 'Elementary Graduate', 'Unemployed', 0, '2024-01-22 06:15:14', '2024-01-22 06:15:14'),
-	(5, 'qweqweqwe', 'qeqe', 'qweqeq', 'qweqe', 3, 'Female', 'HighSchool Level', 'Unemployed', 0, '2024-01-22 06:16:18', '2024-01-22 06:16:18'),
-	(6, 'zczczczxczx', '', 'czxczxczxczxc', '', 2, 'Male', 'HighSchool Level', 'Employed', 0, '2024-01-22 06:16:54', '2024-01-22 06:16:54');
+	(35, 'da', 'das', 'das', 'das', 5, 'Male', 'Doctorate Degree', 'Employed', 1, '2024-02-08 01:26:03', '2024-02-08 01:26:03'),
+	(36, 'dasd', 'dsad', 'dasd', 'da', 3, 'Female', 'College Level', 'Unemployed', 2, '2024-02-08 01:29:29', '2024-02-08 01:29:29'),
+	(37, 'da', 'das', 'das', 'd', 3, 'Male', 'Masters Degree', 'Unemployed', 0, '2024-02-08 01:30:33', '2024-02-08 01:30:33'),
+	(38, 'zcxxz', 'zcxzxczxc', 'zxczxc', 'zxczxc', 3, 'Male', 'Elementary Graduate', 'Student', 0, '2024-02-08 19:46:33', '2024-02-08 19:46:33'),
+	(39, 'asdasd', 'zcxzxcasdasdzxc', 'asdasd', '', 3, 'Others', 'HighSchool Graduate', 'Student', 2, '2024-02-08 19:47:08', '2024-02-08 19:47:08'),
+	(40, 'asdasd', 'zczxczxc', 'asdasd', '', 3, 'Male', 'HighSchool Level', 'Student', 0, '2024-02-08 20:02:46', '2024-02-08 20:02:46'),
+	(41, 'qweqwe', '', 'qweqweqwe', '', 5, 'Male', 'Elementary Graduate', 'Student', 1, '2024-02-08 20:03:06', '2024-02-08 20:03:06'),
+	(42, 'xczxc', '', 'zxcxzc', '', 3, 'Male', 'HighSchool Level', 'Unemployed', 3, '2024-02-08 20:03:52', '2024-02-08 20:03:52'),
+	(43, 'xzcxzcxc', '', 'ss', '', 3, 'Male', 'HighSchool Level', 'Unemployed', 0, '2024-02-08 20:04:23', '2024-02-08 20:04:23'),
+	(44, 'asd', '', 'zxc', '', 3, 'Others', 'College Level', 'Retired', 0, '2024-02-08 20:08:38', '2024-02-08 20:08:38'),
+	(45, 'cvcv', '', 'cvcvcv', '', 2, 'Female', 'HighSchool Level', 'Student', 0, '2024-02-08 20:15:51', '2024-02-08 20:15:51'),
+	(46, 'da', '', 'zcx', '', 5, 'Male', 'HighSchool Level', 'Unemployed', 1, '2024-02-08 20:22:00', '2024-02-08 20:22:00'),
+	(47, 'qwesad', '', 'xzc', '', 3, 'Female', 'Vocational', 'Employed', 0, '2024-02-08 20:23:45', '2024-02-08 20:23:45'),
+	(48, 'please gumana kana', 'nakikiusap ako', 'for the love of god', 'please work', 3, 'Male', 'Elementary Graduate', 'Unemployed', 0, '2024-02-08 20:25:28', '2024-02-08 20:25:28'),
+	(49, 'Gumana ka', 'Please', 'For the love of our beloved maker', 'Please work', 2, 'Female', 'Elementary Graduate', 'Employed', 2, '2024-02-08 20:26:36', '2024-02-08 20:26:36'),
+	(50, 'zxc', 'zc', 'zxczxc', '', 5, 'Female', 'Elementary Graduate', 'Unemployed', 1, '2024-02-08 20:27:33', '2024-02-08 20:27:33'),
+	(51, 'zcxvxcvcxv', 'zccvcxvcxv', 'zxczxcxcvcxvcxv', 'cxvcxvcx', 3, 'Others', 'Vocational', 'Employed', 0, '2024-02-08 20:28:06', '2024-02-08 20:28:06');
 
 -- Dumping structure for table qcpl_engagement.emoji
 CREATE TABLE IF NOT EXISTS `emoji` (
   `emoji_id` int NOT NULL AUTO_INCREMENT,
   `image_path` varchar(255) NOT NULL,
-  `unicode` varchar(255) NOT NULL,
-  `positive` decimal(4,3) NOT NULL,
-  `neutral` decimal(4,3) NOT NULL,
+  `_char` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `unicode_codepoint` varchar(255) NOT NULL,
+  `occurrences` int NOT NULL,
+  `_position` decimal(4,3) NOT NULL,
   `negative` decimal(4,3) NOT NULL,
+  `neutral` decimal(4,3) NOT NULL,
+  `positive` decimal(4,3) NOT NULL,
   `sentiment_score` decimal(4,3) NOT NULL,
-  `value` varchar(255) NOT NULL,
   `unicode_name` varchar(255) NOT NULL,
+  `unicode_block` varchar(255) NOT NULL,
   `remarks` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`emoji_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table qcpl_engagement.emoji: ~6 rows (approximately)
-INSERT INTO `emoji` (`emoji_id`, `image_path`, `unicode`, `positive`, `neutral`, `negative`, `sentiment_score`, `value`, `unicode_name`, `remarks`, `created_at`, `updated_at`) VALUES
-	(1, 'public/assets/images/EMOJI/1.png', '0x1f600', 0.654, 0.260, 0.086, 0.568, 'Very Satisfied', 'GRINNING FACE', 'Often conveys general pleasure and good cheer or humor.', '2024-01-22 08:28:28', '2024-01-22 14:49:40'),
-	(2, 'public/assets/images/EMOJI/2.png', '0x1f60a', 0.704, 0.237, 0.060, 0.644, 'Satisfied', 'SMILING FACE WITH SMILING EYES', 'Often expresses genuine happiness and warm, positive feelings.', '2024-01-22 08:36:20', '2024-01-22 14:49:48'),
-	(3, 'public/assets/images/EMOJI/3.png', '0x1f610', 0.165, 0.282, 0.553, -0.388, 'Neutral', 'NEUTRAL FACE', 'Often used to convey mild irritation and concern or a deadpan sense of humor.', '2024-01-22 08:39:26', '2024-01-22 14:49:54'),
-	(4, 'public/assets/images/EMOJI/4.png', '0x1f614', 0.318, 0.219, 0.464, -0.146, 'Dissatisfied', 'PENSIVE FACE', 'May convey a variety of sad emotions, including feeling disappointed, hurt, or lonely', '2024-01-22 08:42:19', '2024-01-22 14:50:04'),
-	(5, 'public/assets/images/EMOJI/5.png', '0x1f620', 0.265, 0.172, 0.564, -0.299, 'Very Dissatisfied', 'ANGRY FACE', 'Conveys varying degrees of anger, from grumpiness and irritation to disgust and outrage.', '2024-01-22 08:43:54', '2024-01-22 14:50:16'),
-	(6, 'test', '0', 0.000, 0.000, 0.000, 0.000, 'test', 'test', 'test', '2024-01-23 00:16:24', '2024-01-23 00:16:24');
+-- Dumping data for table qcpl_engagement.emoji: ~5 rows (approximately)
+INSERT INTO `emoji` (`emoji_id`, `image_path`, `_char`, `image`, `unicode_codepoint`, `occurrences`, `_position`, `negative`, `neutral`, `positive`, `sentiment_score`, `unicode_name`, `unicode_block`, `remarks`, `created_at`, `updated_at`) VALUES
+	(44, '../../public/assets/images/emojis/ANGRY FACE.png', '😠', '😠', '0x1f620', 288, 0.849, 0.564, 0.172, 0.265, -0.299, 'ANGRY FACE', 'Emoticons', '', '2024-02-08 19:02:55', '2024-02-08 19:02:55'),
+	(45, '../../public/assets/images/emojis/WORRIED FACE.png', '😟', '😟', '0x1f61f', 80, 0.824, 0.349, 0.229, 0.422, 0.072, 'WORRIED FACE', 'Emoticons', '', '2024-02-08 19:05:48', '2024-02-08 19:05:48'),
+	(46, '../../public/assets/images/emojis/NEUTRAL FACE.png', '😐', '😐', '0x1f610', 270, 0.883, 0.553, 0.282, 0.165, -0.388, 'NEUTRAL FACE', 'Emoticons', '', '2024-02-08 19:07:30', '2024-02-08 19:07:30'),
+	(47, '../../public/assets/images/emojis/SMILING FACE WITH SMILING EYES.png', '😊', '😊', '0x1f60a', 3186, 0.813, 0.060, 0.237, 0.704, 0.644, 'SMILING FACE WITH SMILING EYES', 'Emoticons', '', '2024-02-08 19:09:22', '2024-02-08 19:09:22'),
+	(48, '../../public/assets/images/emojis/SMILING FACE WITH OPEN MOUTH AND SMILING EYES.png', '😄', '😄', '0x1f604', 1398, 0.795, 0.137, 0.305, 0.558, 0.421, 'SMILING FACE WITH OPEN MOUTH AND SMILING EYES', 'Emoticons', '', '2024-02-08 19:09:55', '2024-02-08 19:09:55');
 
 -- Dumping structure for table qcpl_engagement.feedback
 CREATE TABLE IF NOT EXISTS `feedback` (
@@ -104,7 +120,7 @@ CREATE TABLE IF NOT EXISTS `feedback` (
   CONSTRAINT `feedback_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `client` (`client_id`) ON DELETE SET NULL ON UPDATE CASCADE,
   CONSTRAINT `feedback_ibfk_2` FOREIGN KEY (`question_id`) REFERENCES `questions` (`question_id`) ON DELETE SET NULL ON UPDATE CASCADE,
   CONSTRAINT `feedback_ibfk_3` FOREIGN KEY (`emoji_id`) REFERENCES `emoji` (`emoji_id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Dumping data for table qcpl_engagement.feedback: ~0 rows (approximately)
 
@@ -172,11 +188,19 @@ CREATE TABLE IF NOT EXISTS `queue` (
   `queue_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`queue_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table qcpl_engagement.queue: ~1 rows (approximately)
+-- Dumping data for table qcpl_engagement.queue: ~9 rows (approximately)
 INSERT INTO `queue` (`queue_id`, `total_queue`, `queue_date`, `updated_at`) VALUES
-	(1, 5, '2024-01-21 16:00:00', '2024-01-22 06:16:54');
+	(1, 5, '2024-01-21 16:00:00', '2024-01-22 06:16:54'),
+	(2, 2, '2024-01-22 16:00:00', '2024-01-23 03:41:27'),
+	(3, 11, '2024-01-28 16:00:00', '2024-01-29 08:59:53'),
+	(4, 7, '2024-01-29 16:00:00', '2024-01-30 07:39:10'),
+	(5, 1, '2024-01-31 16:00:00', '2024-02-01 05:45:13'),
+	(6, 3, '2024-02-05 16:00:00', '2024-02-06 07:35:00'),
+	(7, 1, '2024-02-06 16:00:00', '2024-02-07 05:24:15'),
+	(8, 6, '2024-02-07 16:00:00', '2024-02-08 01:30:33'),
+	(9, 14, '2024-02-08 16:00:00', '2024-02-08 20:28:06');
 
 -- Dumping structure for table qcpl_engagement.queue_details
 CREATE TABLE IF NOT EXISTS `queue_details` (
@@ -185,25 +209,25 @@ CREATE TABLE IF NOT EXISTS `queue_details` (
   `queue_number` varchar(255) NOT NULL,
   `service` varchar(255) NOT NULL,
   `status` tinyint(1) DEFAULT '0',
+  `entry_check` tinyint(1) DEFAULT '1',
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`qd_id`),
   KEY `client_id` (`client_id`),
   CONSTRAINT `queue_details_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `client` (`client_id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=80 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table qcpl_engagement.queue_details: ~10 rows (approximately)
-INSERT INTO `queue_details` (`qd_id`, `client_id`, `queue_number`, `service`, `status`, `created_at`, `updated_at`) VALUES
-	(1, 1, 'P-00001', 'NBI', 0, '2024-01-22 05:04:14', '2024-01-22 05:05:58'),
-	(2, 1, 'P-00002', 'Police', 0, '2024-01-22 05:33:11', '2024-01-22 05:33:11'),
-	(5, 4, 'P-00003', 'NBI', 0, '2024-01-22 06:15:14', '2024-01-22 06:15:14'),
-	(6, 4, 'P-00003', 'Police', 0, '2024-01-22 06:15:14', '2024-01-22 06:15:14'),
-	(7, 4, 'P-00003', 'test1', 0, '2024-01-22 06:15:14', '2024-01-22 06:15:14'),
-	(8, 4, 'P-00003', 'test2', 0, '2024-01-22 06:15:14', '2024-01-22 06:15:14'),
-	(9, 4, 'P-00003', 'test3', 0, '2024-01-22 06:15:14', '2024-01-22 06:15:14'),
-	(10, 5, 'P-00004', 'NBI', 0, '2024-01-22 06:16:18', '2024-01-22 06:16:18'),
-	(11, 6, 'N-00005', 'NBI', 0, '2024-01-22 06:16:54', '2024-01-22 06:16:54'),
-	(12, 6, 'N-00005', 'Police', 0, '2024-01-22 06:16:54', '2024-01-22 06:16:54');
+-- Dumping data for table qcpl_engagement.queue_details: ~9 rows (approximately)
+INSERT INTO `queue_details` (`qd_id`, `client_id`, `queue_number`, `service`, `status`, `entry_check`, `created_at`, `updated_at`) VALUES
+	(65, 40, 'N-00001', 'NBI', 0, 1, '2024-02-08 20:02:46', '2024-02-08 20:02:46'),
+	(66, 40, 'N-00001', 'Police', 0, 1, '2024-02-08 20:02:46', '2024-02-08 20:02:46'),
+	(67, 41, 'P-00001', 'Police', 0, 1, '2024-02-08 20:03:06', '2024-02-08 20:03:06'),
+	(68, 42, 'P-00002', 'Police', 0, 1, '2024-02-08 20:03:52', '2024-02-08 20:03:52'),
+	(75, 48, 'N-00002', 'Police', 0, 1, '2024-02-08 20:25:28', '2024-02-08 20:25:28'),
+	(76, 49, 'P-00003', 'NBI', 0, 1, '2024-02-08 20:26:36', '2024-02-08 20:26:36'),
+	(77, 49, 'P-00003', 'Police', 0, 1, '2024-02-08 20:26:36', '2024-02-08 20:26:36'),
+	(78, 50, 'P-00004', 'NBI', 0, 1, '2024-02-08 20:27:33', '2024-02-08 20:27:33'),
+	(79, 51, 'N-00003', 'PSA', 0, 1, '2024-02-08 20:28:06', '2024-02-08 20:28:06');
 
 -- Dumping structure for table qcpl_engagement.service
 CREATE TABLE IF NOT EXISTS `service` (
@@ -230,12 +254,13 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `username` (`username`),
   KEY `user_role_id` (`user_role_id`),
   CONSTRAINT `users_ibfk_1` FOREIGN KEY (`user_role_id`) REFERENCES `user_role` (`user_role_id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table qcpl_engagement.users: ~2 rows (approximately)
+-- Dumping data for table qcpl_engagement.users: ~3 rows (approximately)
 INSERT INTO `users` (`user_id`, `user_role_id`, `username`, `password`, `is_active`, `last_login`, `created_at`, `updated_at`) VALUES
-	(4, 1, 'admin', '$2y$10$MRaoOOnRQ69KTufCOTKo3u87J0E41e3XIIvYSr/3vG58uJ6NDtWWS', 1, '0000-00-00 00:00:00', '2023-12-28 02:52:00', '2023-12-28 02:52:00'),
-	(5, 2, 'staff', '$2y$10$pk3hTpWf00wNWmTR6XtSY.mOzNX3T8Gy.tmlArEtxEY3djTzoCnE6', 1, '0000-00-00 00:00:00', '2023-12-28 02:53:48', '2023-12-28 02:53:48');
+	(4, 1, 'admin', '$2y$10$MRaoOOnRQ69KTufCOTKo3u87J0E41e3XIIvYSr/3vG58uJ6NDtWWS', 1, '2024-02-08 10:58:50', '2023-12-28 02:52:00', '2024-02-08 10:58:50'),
+	(5, 2, 'staff', '$2y$10$pk3hTpWf00wNWmTR6XtSY.mOzNX3T8Gy.tmlArEtxEY3djTzoCnE6', 1, '0000-00-00 00:00:00', '2023-12-28 02:53:48', '2023-12-28 02:53:48'),
+	(7, 1, 'bogart123', '$2y$10$uH24T.acTZWVm3Mt8fjO9.d.xV3jKgXxlJR/ch8swhPZKEImZgLw6', 1, '0000-00-00 00:00:00', '2024-01-23 02:35:23', '2024-01-23 02:35:23');
 
 -- Dumping structure for table qcpl_engagement.user_role
 CREATE TABLE IF NOT EXISTS `user_role` (
