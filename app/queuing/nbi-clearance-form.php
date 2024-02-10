9bfd2154894bccd470ba198ea5c8221af5ee4c7a
n67h <night67hunter@gmail.com>
2024-02-10 23:53:56 +0800

    Display client details

--- app/queuing/nbi-clearance-form.php
+++ app/queuing/nbi-clearance-form.php
@@ -1,5 +1,45 @@
 <?php
     require_once('../core/init.php');
+    if(isset($_SESSION['user_id'])){
+        if(isset($_GET['queue_no']) && isset($_GET['client_id']) && isset($_GET['birthdate'])){
+            $queue_no = $_GET['queue_no'];
+            $client_id = $_GET['client_id'];
+            $birthdate = $_GET['birthdate'];
+
+            $sql = "SELECT * FROM client WHERE client_id = $client_id;";
+            $result = mysqli_query($conn, $sql);
+            if(mysqli_num_rows($result) > 0){
+                while($row = mysqli_fetch_assoc($result)){
+                    $f_name = $row['f_name'];
+                    $m_name = $row['m_name'];
+                    $l_name = $row['l_name'];
+                    $suffix = $row['suffix'];
+                    $age_id = $row['age_id'];
+                    $gender = $row['gender'];
+                    $education = $row['education'];
+                    $occupation = $row['occupation'];
+                    $status = $row['status'];
+                    $created_at = $row['created_at'];
+
+                    if($status == 0){
+                        $new_status = 'Normal';
+                    }elseif($status == 1){
+                        $new_status = 'Senior';
+                    }elseif($status == 2){
+                        $new_status = 'PWD';
+                    }elseif($status == 3){
+                        $new_status = 'Pregnant';
+                    }
+                }
+            }else{
+                header('location: ../../public/index.php');
+                die();
+            }
+        }
+    }else{
+        header('location: ../../public/index.php');
+        die();
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -52,13 +92,13 @@ select -->
                                 <div class="col-6">
                                     <form>
                                         <label class="form-label" for="first-name">First Name:</label>
-                                        <input type="text" class="form-control" id="first-name" placeholder="">
+                                        <input type="text" class="form-control" id="first-name" placeholder="" value="<?= $f_name ?>">
                                     </form>
                                 </div>
                                 <div class="col-6">
                                     <form>
                                         <label class="form-label" for="middle-name">Middle Name:</label>
-                                        <input type="text" class="form-control" id="middle-name" placeholder="">
+                                        <input type="text" class="form-control" id="middle-name" placeholder="" value="<?= $m_name ?>">
                                     </form>
                                 </div>
                             </div>
@@ -68,13 +108,13 @@ select -->
                                 <div class="col-7">
                                     <form>
                                         <label class="form-label" for="last-name">Last Name:</label>
-                                        <input type="text" class="form-control" id="last-name" placeholder="">
+                                        <input type="text" class="form-control" id="last-name" placeholder="" value="<?= $l_name ?>">
                                     </form>
                                 </div>
                                 <div class="col-5">
                                     <form>
                                         <label class="form-label" for="suffix">Suffix: <span class="fst-italic">(Jr./Sr./II/III)</span></label>
-                                        <input type="text" class="form-control" id="suffix" placeholder="">
+                                        <input type="text" class="form-control" id="suffix" placeholder="" value="<?= $suffix ?>">
                                     </form>
                                 </div>
                             </div>
@@ -84,7 +124,7 @@ select -->
                                 <div class="col-6">
                                     <form>
                                         <label for="gender" class="form-label">Gender:</label>
-                                        <input type="text" class="form-control" id="gender">
+                                        <input type="text" class="form-control" id="gender" value="<?= $gender ?>">
                                     </form>
                                 </div>
                                 <div class="col-1">
@@ -108,8 +148,8 @@ select -->
                             <div class="row">
                                 <div class="col-5">
                                     <form>
-                                        <label for="birthday" class="form-label mt-3">Birthday:</label>
-                                        <input type="date" class="form-control" id="birthday">
+                                        <label for="birthday" class="form-label mt-3">Birthdate:</label>
+                                        <input type="date" class="form-control" id="birthday" value="<?= $birthdate ?>">
                                     </form>
                                 </div>
                                 <div class="col-7">
@@ -161,7 +201,7 @@ select -->
                             <div class="row">
                                 <form class="d-flex align-items-center">
                                     <label for="status" class="form-label me-2">Status:</label>
-                                    <input type="text" class="form-control" id="status" value="">
+                                    <input type="text" class="form-control" id="status" value="<?= $new_status ?>">
                                 </form>
                             </div>
 
@@ -261,7 +301,7 @@ select -->
                                     <div class="subtop-1">
                                         <form>
                                             <label for="educ" class="form-label">Educational Attainment:</label>
-                                            <input type="text" class="form-control" id="educ" value="SAMPLE" disabled>
+                                            <input type="text" class="form-control" id="educ" value="<?= $education ?>">
                                         </form>
                                     </div>
                                 </div>
@@ -407,28 +447,21 @@ select -->
                 <div class="col-2" style>
                     <div class="row">
                         <div class="col" style="text-align: center;">
-                            <h4>Que Number</h4>
+                            <h4>Queue Number</h4>
                             <div class="card">
-                                <h4>N-000006</h4>
+                                <h4><?= $queue_no ?></h4>
                             </div>
                         </div>
                         <div class="sub-information">
                             <h4>Main Library</h4>
-                            <h4>02/08/2024:12:00am</h4>
+                            <h4 class="ps-4"><?= $created_at ?></h4>
                             <h4>2 Gov. ID</h4>
                             <p>1.___________
                             <p>
                             <p>2.___________
                             <p>
-
                         </div>
-
                     </div>
-
-
-
-                    <!-- <label for="birthday" class="form-label">Birthday:</label> -->
-
                 </div>
             </div>
         </div>
@@ -444,5 +477,8 @@ select -->
     </style>
     <!-- style="text-align: center;" -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script>
+        window.onload = function() { window.print(); }
+    </script>
 </body>
 </html>
