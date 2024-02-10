116eb07c5b488216670304cdd8bd3e674b1aaf32
n67h <night67hunter@gmail.com>
2024-02-11 00:52:21 +0800

    Edit question type

--- app/admin/question-type.php
+++ app/admin/question-type.php
@@ -157,7 +157,7 @@
                                                     <td class="text-center"><?= $created_at ?></td>
                                                     <td class="text-center"><?= $updated_at ?></td>
                                                     <td class="text-center">
-                                                        <a class="btn btn-sm btn-success edit" href="#" data-bs-toggle="modal" data-bs-target="#edit_service_modal"><i class="fa-solid fa-pen-to-square"></i></a>  
+                                                        <a class="btn btn-sm btn-success edit" href="#" data-bs-toggle="modal" data-bs-target="#edit_question_type_modal"><i class="fa-solid fa-pen-to-square"></i></a>  
                                                     </td>
                                                 </tr>
                                     <?php
@@ -186,6 +186,69 @@
                     <!-- end of second container -->
                 </div>
                 <!-- end of first row -->
+
+                <!-- start of edit question type modal -->
+                <div class="modal fade" id="edit_question_type_modal">
+                    <!-- start of edit modal dialog -->
+                    <div class="modal-dialog modal-lg modal-dialog-centered">
+                        <!-- start of edit modal content -->
+                        <div class="modal-content">
+                            <!-- start of modal header -->
+                            <div class="modal-header bg-dark border-0">
+                                <h4 class="modal-title text-white">Edit Question Type</h4>
+                                <button type="button" class="btn btn-danger close" data-bs-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true"><i class="fa-solid fa-xmark"></i></span>
+                                </button>
+                            </div>
+                            <!-- end of modal header -->
+                            <!-- start of edit modal form -->
+                            <form action="functions/edit-question-type.php" method="post">
+                                <!-- start of edit modal body -->                
+                                <div class="modal-body">
+                                    <!-- start of edit modal row -->
+                                    <div class="row">
+                                        <!-- start of edit modal col -->
+                                        <div class="col-md-12">
+                                            <!-- start of edit modal card -->
+                                            <div class="card card-primary">
+                                                <!-- start of edit modal card body -->
+                                                <div class="card-body">
+                                                    <!-- start of edit modal row -->
+                                                    <div class="row">
+                                                        <input type="hidden" class="form-control" name="edit_qt_id" id="edit_qt_id" value="">
+                                                        
+                                                        <div class="col-md-12 col-6 mt-3">
+                                                            <div class="form-group">
+                                                                <label for="edit_question_type" class="ps-2 pb-2">Question Type</label>
+                                                                <input type="text" class="form-control" name="edit_question_type" id="edit_question_type" value="" required>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <!-- end of edit modal row -->
+                                                </div>
+                                                <!-- end of edit modal card body -->
+                                                <!-- start of edit modal footer -->
+                                                <div class="modal-footer justify-content-end">
+                                                    <button type="submit" name="edit_qt" class="btn btn-success">Save Changes</button>
+                                                </div>
+                                                <!-- end of edit modal footer -->
+                                            </div>
+                                            <!-- end of edit modal card -->
+                                        </div>
+                                        <!-- end of edit modal col -->
+                                    </div>
+                                    <!-- end of edit modal row -->
+                                </div>
+                                <!-- end of edit modal body -->                
+                            </form>
+                            <!-- end of edit modal form -->
+                        </div>
+                        <!-- end of edit modal content -->
+                    </div>
+                    <!-- end of edit modal dialog -->
+                </div>
+                <!-- end of edit question type modal -->
+
             </div>
             <!-- end of card body -->
         </div>
