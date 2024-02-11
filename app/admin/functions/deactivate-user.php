f4b682346cc11600b9eea8b1cf5c9fdc6e02592f
BERT151 <warnethalarcon15@gmail.com>
2024-02-12 02:24:59 +0800

    Update submit button name

--- app/admin/functions/deactivate-user.php
+++ app/admin/functions/deactivate-user.php
@@ -1,7 +1,7 @@
 <?php
     require_once '../../core/init.php';
 
-    if(isset($_POST['deactivate'])){
+    if(isset($_POST['deactivate_user'])){
         $deactivate_user_id = mysqli_real_escape_string($conn, $_POST['deactivate_user_id']);
 
         $sql = "UPDATE users SET is_active = 0 WHERE user_id = $deactivate_user_id;";
