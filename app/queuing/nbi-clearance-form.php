3ea9a7e73f52dd93be596264ea955ae004b7c6d4
BERT151 <warnethalarcon15@gmail.com>
2024-02-12 05:28:07 +0800

    add after print back to home page

--- app/queuing/nbi-clearance-form.php
+++ app/queuing/nbi-clearance-form.php
@@ -465,7 +465,17 @@ select -->
     <!-- style="text-align: center;" -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script>
-        window.onload = function() { window.print(); }
+    window.onload = function() {
+    window.print();
+    };
+
+    window.onafterprint = function(e) {
+        closePrintView();
+    };
+
+    function closePrintView() {
+        window.location.href = 'queue.php';   
+    }
     </script>
 </body>
 </html>
--- app/queuing/other-form.php
+++ app/queuing/other-form.php
@@ -57,7 +57,7 @@
     <div class="formbg">
     <div class="row">
         <div class="col" style="text-align: center;">
-            <h1>New Form</h1>
+            <h1>Transaction Form</h1>
         </div>
     </div>
 
@@ -158,5 +158,18 @@
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script>
+    window.onload = function() {
+    window.print();
+};
+
+window.onafterprint = function(e) {
+    closePrintView();
+};
+
+function closePrintView() {
+    window.location.href = 'queue.php';   
+}
+</script>
 </body>
 </html>
