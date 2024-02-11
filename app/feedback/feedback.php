5f116745d9f3dcf7d61becaff7ea4af5dbd65e08
BERT151 <warnethalarcon15@gmail.com>
2024-02-12 03:58:59 +0800

    Remove feedback-complete.php

--- app/feedback/feedback.php
+++ app/feedback/feedback.php
@@ -125,10 +125,6 @@ $client_id = isset($_POST['id']) ? $_POST['id'] : null;
             </div>
             <img src="../../public/assets/images/qcplLogo.png" alt="">
         </div>
-        <br>
-        <br>
-        <br>
-        <br>
 
         <!-- Start of feedback form -->
         <form action="" method="post">
@@ -144,7 +140,7 @@ $client_id = isset($_POST['id']) ? $_POST['id'] : null;
                     if (isset($_POST['search'])) {
                         $id = $_POST['id'];
 
-                        $sql = "SELECT CONCAT(LEFT(client.f_name, 1), REPEAT('*', LENGTH(client.f_name) - 1), ' ', LEFT(client.l_name, 1), REPEAT('*', LENGTH(client.l_name) - 1)) AS masked_name, queue_details.queue_number, queue_details.client_id, queue_details.created_at AS created_at FROM client INNER JOIN queue_details ON client.client_id = queue_details.client_id WHERE queue_number = '$id' AND DATE(queue_details.created_at) = CURDATE() AND queue_details.entry_check = 1 LIMIT 1";
+                        $sql = "SELECT CONCAT(LEFT(client.f_name, 1), REPEAT('*', LENGTH(client.f_name) - 1), ' ', LEFT(client.l_name, 1), REPEAT('*', LENGTH(client.l_name) - 1)) AS masked_name, queue_details.queue_number, queue_details.client_id, queue_details.created_at AS created_at FROM client INNER JOIN queue_details ON client.client_id = queue_details.client_id WHERE queue_number = '$id' AND DATE(queue_details.created_at) = CURDATE() LIMIT 1";
                         $res = mysqli_query($conn, $sql);
 
                         if ($row = mysqli_fetch_array($res)) {
@@ -167,7 +163,7 @@ $client_id = isset($_POST['id']) ? $_POST['id'] : null;
                                             <path fill="#fff" d="M449.07,399.08,278.64,82.58c-12.08-22.44-44.26-22.44-56.35,0L51.87,399.08A32,32,0,0,0,80,446.25H420.89A32,32,0,0,0,449.07,399.08Zm-198.6-1.83a20,20,0,1,1,20-20A20,20,0,0,1,250.47,397.25ZM272.19,196.1l-5.74,122a16,16,0,0,1-32,0l-5.74-121.95v0a21.73,21.73,0,0,1,21.5-22.69h.21a21.74,21.74,0,0,1,21.73,22.7Z" />
                                         </svg>
                                     </div>
-                                    <p>A Danger Message</p>
+                                    <p>Sorry, No Data was found.</p>
                                 </div>
                                 <button class="close">
                                     <svg height="18px" id="Layer_1" style="enable-background:new 0 0 512 512;" version="1.1" viewBox="0 0 512 512" width="18px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
@@ -298,7 +294,6 @@ $client_id = isset($_POST['id']) ? $_POST['id'] : null;
 
                 // Redirect to the feedback-complete.php page after showing the second modal
                 setTimeout(function() {
-                    window.location.href = './includes/feedback-complete.php?client_id=<?= $client_id ?>';
                 }, 3000); // Adjust the delay time as needed
             });
 
