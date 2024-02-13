972824d90be7623278f197f3d6b4c4f131135b09
n67h <night67hunter@gmail.com>
2024-02-13 10:46:28 +0800

    Remove update status

--- app/feedback/feedback-form.php
+++ app/feedback/feedback-form.php
@@ -6,8 +6,6 @@
     }else{
         if(isset($_GET['client_id'])){
             $client_id = $_GET['client_id'];
-            $sql = "UPDATE queue_details SET status = 1 WHERE client_id = $client_id;";
-            mysqli_query($conn, $sql);
         }else{
             header('location: feedback.php');
             die();
