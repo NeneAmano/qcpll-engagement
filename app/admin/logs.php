a8021f9717128ff85417846393552ce442118720
n67h <night67hunter@gmail.com>
2024-02-16 16:07:17 +0800

    Add icon

--- app/admin/logs.php
+++ app/admin/logs.php
@@ -10,7 +10,9 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../../public/assets/images/qcplLogo.png" type="image/x-icon">
     <title>Logs</title>
     <?php
         require_once 'includes/sidebar.php';
