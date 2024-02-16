73c33b8fb384f1b7524277a1223e0378cf8feeec
n67h <night67hunter@gmail.com>
2024-02-16 19:02:56 +0800

    Change sidebar link for questions

--- app/admin/includes/sidebar.php
+++ app/admin/includes/sidebar.php
@@ -65,7 +65,7 @@
                         <li class=""><a href="logs.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-book"></i><span class="ps-2">Logs</span></a></li>
                         <li class=""><a href="analytics.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-chart-simple"></i><span class="ps-2">Analytics</span></a></li>
                         <li class=""><a href="feedback.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-chart-pie"></i><span class="ps-2">Feedbacks</span></a></li>
-                        <li class=""><a href="questions.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-circle-question"></i><span class="ps-2">Questions</span></a></li>
+                        <li class=""><a href="questions.php?archived-records=no" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-circle-question"></i><span class="ps-2">Questions</span></a></li>
                         <li class=""><a href="reports.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-print"></i><span class="ps-2">Reports</span></a></li>
                         <li class=""><a href="emoji.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-regular fa-face-laugh-beam"></i><span class="ps-2">Emoji</span></a></li>
                         <li class=""><a href="queue-screen.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-display"></i><span class="ps-2">Queue Monitoring</span></a></li>
