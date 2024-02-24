c5c1881281fe801fc0f5ea9806ec83455bd7b849
n67h <night67hunter@gmail.com>
2024-02-24 22:04:28 +0800

    Format file and remove white space

--- app/admin/history.php
+++ app/admin/history.php
@@ -2,16 +2,15 @@
 
 
 <?php
-require_once('../core/init.php');
-ob_start();
-if (($user_role_id_session !== 1)) {
-    header('location: login.php?error=accessdenied');
-    die();
-}
+    require_once('../core/init.php');
+    ob_start();
+    if (($user_role_id_session !== 1)) {
+        header('location: login.php?error=accessdenied');
+        die();
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
