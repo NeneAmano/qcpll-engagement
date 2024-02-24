8c20ff6413504e7ac9fa573041797cc8b9b8917e
n67h <night67hunter@gmail.com>
2024-02-24 22:04:14 +0800

    Format file and remove white space

--- app/admin/feedback.php
+++ app/admin/feedback.php
@@ -1,14 +1,13 @@
 <?php
-require_once('../core/init.php');
-ob_start();
-if (($user_role_id_session !== 1)) {
-    header('location: login.php?error=accessdenied');
-    die();
-}
+    require_once('../core/init.php');
+    ob_start();
+    if (($user_role_id_session !== 1)) {
+        header('location: login.php?error=accessdenied');
+        die();
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -66,42 +65,9 @@ if (($user_role_id_session !== 1)) {
     ?>
     <!-- start of main section container -->
     <div class="container-fluid mt-3">
-
-        <!-- filter by today-->
-        <button type="button" class="btn btn-success mb-3 mt-3 me-2" data-bs-toggle="" data-bs-target=""><a href="feedback.php?filter=today" class="text-decoration-none text-light">Today</a></button>
-
-        <!-- filter by 7 days -->
-        <button type="button" class="btn btn-success mb-3 mt-3 me-2" data-bs-toggle="" data-bs-target=""><a href="feedback.php?filter=7days" class="text-decoration-none text-light">Past 7 Days</a></button>
-
-        <!-- filter by month -->
-        <div class="dropdown">
-            <button class="btn btn-success dropdown-toggle mb-3 mt-3 me-2">Filter by Month</button>
-            <div class="dropdown-content">
-                <?php
-                    for ($month = 1; $month <= 12; $month++) {
-                        $month_name = date("F", mktime(0, 0, 0, $month, 1));
-                        echo '<a href="feedback.php?filter=' . $month . '">' . $month_name . '</a>';
-                    }
-                ?>
-            </div>
-        </div>
-
-        <!-- filter by year -->
-        <div class="dropdown">
-            <button class="btn btn-success dropdown-toggle mb-3 mt-3 me-2">Filter by Year</button>
-            <div class="dropdown-content">
-                <?php
-                    $sql_year = "SELECT DISTINCT YEAR(`created_at`) AS year FROM `client` ORDER BY year ASC";
-                    $result_year = mysqli_query($conn, $sql_year);
-
-                    while ($row_year = mysqli_fetch_assoc($result_year)) {
-                        $year = $row_year['year'];
-                        echo '<a href="feedback.php?filter=' . $year . '" class="text-decoration-none text-dark">' . $year . '</a>';
-                    }
-                ?>
-            </div>
-        </div>
-
+        <?php
+            require_once 'includes/filter.php';
+        ?>
         <!-- end of add service modal button -->
         <!-- start of first row -->
         <div class="row">
