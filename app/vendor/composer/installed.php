b0314a4112df336b9b5219cbad7f9997a28c721a
n67h <night67hunter@gmail.com>
2024-02-26 16:44:27 +0800

    Add davmixcool php sentiment analyzer

---
+++ app/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd148b15b1d0f8744237ec0948d65e534e1c3bd6a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd148b15b1d0f8744237ec0948d65e534e1c3bd6a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'davmixcool/php-sentiment-analyzer' => array(
+            'pretty_version' => '1.2.2',
+            'version' => '1.2.2.0',
+            'reference' => 'd7503b68313c62eee3ed0ac9774c0a84d818a405',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../davmixcool/php-sentiment-analyzer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
