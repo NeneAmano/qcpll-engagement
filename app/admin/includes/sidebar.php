167465595435a8c27369ceba756c4acb734363b3
n67h <night67hunter@gmail.com>
2024-02-27 02:34:55 +0800

    Change link for logs

--- app/admin/includes/sidebar.php
+++ app/admin/includes/sidebar.php
@@ -64,7 +64,7 @@
                         <li class=""><a href="users.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-user"></i><span class="ps-2">Users</span></a></li>
                         <li class=""><a href="clients.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-users"></i><span class="ps-2">Clients</span></a></li>
                         <li class=""><a href="history.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-book-open"></i><span class="ps-2">History</span></a></li>
-                        <li class=""><a href="nbi_logs.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-book"></i><span class="ps-2">Logs</span></a></li>
+                        <li class=""><a href="nbi-logs.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-book"></i><span class="ps-2">Logs</span></a></li>
                         <li class=""><a href="analytics.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-chart-simple"></i><span class="ps-2">Analytics</span></a></li>
                         <li class=""><a href="feedback.php" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-chart-pie"></i><span class="ps-2">Feedbacks</span></a></li>
                         <li class=""><a href="questions.php?archived-records=no" class="text-decoration-none px-3 py-2 d-block"><i class="fa-solid fa-circle-question"></i><span class="ps-2">Questions</span></a></li>
