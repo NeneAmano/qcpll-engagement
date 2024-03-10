f9e671f007bc875b4c6ef4d253edf50e5c7f023a
n67h <night67hunter@gmail.com>
2024-03-10 20:25:10 +0800

    Create query for logs

--- app/admin/includes/logout.php
+++ app/admin/includes/logout.php
@@ -1,5 +1,11 @@
 <?php
-    session_start();
+    require_once('../../core/init.php');
+    if(isset($_GET['user_id'])){
+        $user_id_session = $_GET['user_id'];
+
+        $logs_sql = "INSERT INTO history_logs (user_id, content, content_id, _action) VALUES ($user_id_session, 'logout', 0, 'Log out');";
+        $logs_result = mysqli_query($conn, $logs_sql);
+    }
     session_unset();
     session_destroy();
     header("Location: ../login.php");
