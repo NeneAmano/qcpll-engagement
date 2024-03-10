864e0cf5cb70f61d41fc4f654bb14c7b90e11a9d
n67h <night67hunter@gmail.com>
2024-03-10 22:04:18 +0800

    Create query logs

--- app/admin/functions/deactivate-user.php
+++ app/admin/functions/deactivate-user.php
@@ -4,8 +4,11 @@
     if(isset($_POST['deactivate_user'])){
         $deactivate_user_id = mysqli_real_escape_string($conn, $_POST['deactivate_user_id']);
 
-        $sql = "UPDATE users SET is_active = 0 WHERE user_id = $deactivate_user_id;";
+        $sql = "UPDATE users SET is_active = 1 WHERE user_id = $deactivate_user_id;";
         if(mysqli_query($conn, $sql)){
+            $logs_sql = "INSERT INTO history_logs (user_id, content, content_id, _action) VALUES ($user_id_session, 'users', $deactivate_user_id, 'Deactivate');";
+            $logs_result = mysqli_query($conn, $logs_sql);
+
             header("location: ../users.php?deactivate=successful");
             die();
         }
