a320dbe627e7fb1812d571aa584530c8692a20b5
BERT151 <warnethalarcon15@gmail.com>
2024-03-12 10:58:22 +0800

    date

--- app/admin/dashboard.php
+++ app/admin/dashboard.php
@@ -312,6 +312,8 @@
     ?>
     <!-- start of main section container -->
     <section>
+        <?= date_default_timezone_get(); ?>
+        <?= "The time is " . date("h:i:sa"); ?>
         <p class="admin-title">WELCOME, ADMIN </p>
         <!-- start of main row -->
         <div class="row">
