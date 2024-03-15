ffbd2127378f5d4131bbed28bc234bd72a943261
Nabszx <151804843+Nabszx@users.noreply.github.com>
2024-03-15 21:58:15 +0800

    Update form.php
    missed the commented script mb

--- app/queuing/form.php
+++ app/queuing/form.php
@@ -400,11 +400,23 @@ if (isset ($_SESSION['user_id'])) {
         </div>
 
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+    <script>
+        window.onload = function () {
+            window.print();
+        };
 
+        window.onafterprint = function (e) {
+            closePrintView();
+        };
+
+        function closePrintView() {
+            window.location.href = 'queue.php';
+        }
+    </script>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-    crossorigin="anonymous"></script>
 
 
 </html>
